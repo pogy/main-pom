d2626f46537ddba3daa753ace83547a543320c88
zhaochuanyin <81682024@qq.com>
2017-04-24 13:54:37 +0800

    开放男女装商品权重

--- web-server/src/main/webapp/sysman/datas/spread_nav_man.js
+++ web-server/src/main/webapp/sysman/datas/spread_nav_man.js
@@ -8,4 +8,14 @@ var navs = [{
 			"icon": "&#xe63c;",
 			"href": "/sysman/goat/locationList.htm?localCodeLike=MAN"
 		}]
+}, {
+	"title": "商品管理",
+	"icon": "fa-cubes",
+	"spread": false,
+	"children": [
+		{
+			"title": "商品权重",
+			"icon": "&#xe63c;",
+			"href": "/sysman/goods/wh_list.htm"
+		}]
 }];
