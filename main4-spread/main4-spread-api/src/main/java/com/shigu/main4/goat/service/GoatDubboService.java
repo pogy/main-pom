d59ddf06657c03249682529de5105aba853c4809
bugzy <xiaochenyoo@sina.com>
2017-05-10 15:29:05 +0800

    test 包内一些更新，无关不需上线

--- main4-spread/main4-spread-api/src/main/java/com/shigu/main4/goat/service/GoatDubboService.java
+++ main4-spread/main4-spread-api/src/main/java/com/shigu/main4/goat/service/GoatDubboService.java
@@ -2,6 +2,9 @@ package com.shigu.main4.goat.service;
 
 import com.shigu.main4.goat.exceptions.GoatException;
 import com.shigu.main4.goat.vo.GoatVO;
+import com.shigu.main4.goat.vo.ItemUpVO;
+
+import java.util.List;
 
 /**
  * 广告对外服务
@@ -9,4 +12,40 @@ import com.shigu.main4.goat.vo.GoatVO;
  */
 public interface GoatDubboService {
     <T extends GoatVO>T selGoatById(Long goatId) throws GoatException;
+    <T extends GoatVO> List<T> selGoatByLocalId(Long localId) throws GoatException;
+
+    /**
+     * 查询真实上款量与虚假上款量
+     * @return 真实上款量与虚假上款量
+     */
+    ItemUpVO selUp(Long goatId);
+
+    /**
+     * 修改虚假上款量
+     * @param num 修改后的虚假上款量
+     */
+    void modifyUp(Long goatId, Integer num);
+
+    /**
+     * 查询权重
+     * @return 商品的权重
+     */
+    Double selWeight(Long goatId);
+
+    /**
+     * 修改权重
+     * @param weight 修改后的权重
+     */
+    void updateWeight(Long goatId, Double weight);
+
+    /**
+     * 发布上线
+     */
+    void publish(GoatVO vo);
+
+    /**
+     * 预发布
+     * @param second 多久后发布以秒为单位
+     */
+    void preparePublish(GoatVO vo, Long second);
 }
--- main4-spread/main4-spread-default/src/main/java/com/shigu/main4/goat/service/impl/GoatDubboServiceImpl.java
+++ main4-spread/main4-spread-default/src/main/java/com/shigu/main4/goat/service/impl/GoatDubboServiceImpl.java
@@ -4,12 +4,17 @@ import com.shigu.main4.goat.exceptions.GoatException;
 import com.shigu.main4.goat.service.GoatDubboService;
 import com.shigu.main4.goat.service.GoatFactory;
 import com.shigu.main4.goat.vo.GoatVO;
+import com.shigu.main4.goat.vo.ItemUpVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 广告对外服务
  * Created by zhaohongbo on 17/5/9.
  */
+@Service("goatDubboService")
 public class GoatDubboServiceImpl implements GoatDubboService {
     @Autowired
     GoatFactory goatFactory;
@@ -18,4 +23,40 @@ public class GoatDubboServiceImpl implements GoatDubboService {
     public <T extends GoatVO> T selGoatById(Long goatId) throws GoatException {
         return goatFactory.selGoatById(goatId);
     }
+
+    @Override
+    public <T extends GoatVO> List<T> selGoatByLocalId(Long localId) throws GoatException {
+        return null;
+    }
+
+    @Override
+    public ItemUpVO selUp(Long goatId) {
+        return null;
+    }
+
+    @Override
+    public void modifyUp(Long goatId, Integer num) {
+
+    }
+
+    @Override
+    public Double selWeight(Long goatId) {
+        return null;
+    }
+
+    @Override
+    public void updateWeight(Long goatId, Double weight) {
+
+    }
+
+    @Override
+    public void publish(GoatVO vo) {
+
+    }
+
+    @Override
+    public void preparePublish(GoatVO vo, Long second) {
+
+    }
+
 }
