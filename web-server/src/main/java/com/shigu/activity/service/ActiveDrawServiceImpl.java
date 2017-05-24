dc1393ccc8c66d252d7b87b1380b7ec228a1f48d
worksoft_zjb <worksoftnet@163.com>
2017-05-24 09:57:38 +0800

    发现好货抽奖数据生成bug修复

--- web-server/src/main/java/com/shigu/activity/service/ActiveDrawServiceImpl.java
+++ web-server/src/main/java/com/shigu/activity/service/ActiveDrawServiceImpl.java
@@ -721,9 +721,15 @@ public class ActiveDrawServiceImpl implements ActiveDrawService{
         }
 
         ActiveDrawRecordExample recordExample = new ActiveDrawRecordExample();
-        recordExample.createCriteria().andUserIdEqualTo(userId).andPemIdEqualTo(activeDrawPem.getId()).andWardNotEqualTo("A4");
+        recordExample.createCriteria().andUserIdEqualTo(userId).andPemIdEqualTo(activeDrawPem.getId()).andWardEqualTo("A4");
         int recordCount = activeDrawRecordMapper.countByExample(recordExample);
         ActiveDrawRecord activeDrawRecord = new ActiveDrawRecord();
+        activeDrawRecord.setUserId(userId);
+        activeDrawRecord.setPemId(activeDrawPem.getId());
+        activeDrawRecord.setEnabled(false);
+        activeDrawRecord.setReceivesYes(false);
+        activeDrawRecord.setCreateTime(new Date());
+        activeDrawRecord.setModifyTime(new Date());
         if(recordCount == 0){
             // 查询发现好货活动的数据
             ActiveDrawGoodsExample drawGoodsExample = new ActiveDrawGoodsExample();
@@ -731,13 +737,7 @@ public class ActiveDrawServiceImpl implements ActiveDrawService{
             List<ActiveDrawGoods> drawGoodsList = activeDrawGoodsMapper.selectByExample(drawGoodsExample);
             List goodsList = BeanMapper.getFieldList(drawGoodsList, "goodsId", List.class);
             Long total = selGoodsupTotal(goodsList, userId, activeDrawPem.getStartTime());
-            activeDrawRecord.setUserId(userId);
-            activeDrawRecord.setPemId(activeDrawPem.getId());
             activeDrawRecord.setDrawStatus(ActiveDrawRecord.DRAW_STATUS_WAIT);
-            activeDrawRecord.setEnabled(false);
-            activeDrawRecord.setReceivesYes(false);
-            activeDrawRecord.setCreateTime(new Date());
-            activeDrawRecord.setModifyTime(new Date());
             if(total >= 3){
                 activeDrawRecord.setWard("A1");
                 String drawCode = StringUtil.str10To37Str();
@@ -767,7 +767,7 @@ public class ActiveDrawServiceImpl implements ActiveDrawService{
         }
 
         ActiveDrawRecordExample recordBExample = new ActiveDrawRecordExample();
-        recordBExample.createCriteria().andUserIdEqualTo(userId).andPemIdEqualTo(activeDrawPem.getId()).andWardNotEqualTo("B2");
+        recordBExample.createCriteria().andUserIdEqualTo(userId).andPemIdEqualTo(activeDrawPem.getId()).andWardEqualTo("B2");
         int recordBCount = activeDrawRecordMapper.countByExample(recordBExample);
         if(recordBCount == 0){
             // 查询每日发现
