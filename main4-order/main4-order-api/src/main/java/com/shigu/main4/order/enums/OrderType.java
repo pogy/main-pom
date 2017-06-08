ec1d6fa8dc33695880953365f13b722234aa42aa
bugzy <xiaochenyoo@sina.com>
2017-06-08 20:41:12 +0800

    MAIN_SITE4-311 添加包材，实现com.shigu.main4.order.model.ItemOrder#addPackage

--- main4-order/main4-order-api/src/main/java/com/shigu/main4/order/enums/OrderType.java
+++ main4-order/main4-order-api/src/main/java/com/shigu/main4/order/enums/OrderType.java
@@ -5,5 +5,18 @@ package com.shigu.main4.order.enums;
  * Created by zhaohongbo on 17/5/31.
  */
 public enum OrderType {
-    XZ //星座商品的订单
+    XZ(1); //星座商品的订单
+
+    public int type;
+    OrderType(int type) {
+        this.type = type;
+    }
+
+    public static OrderType typeOf(int type) {
+        for (OrderType orderType : values()) {
+            if (type == orderType.type)
+                return orderType;
+        }
+        throw new IllegalArgumentException(String.format("type[%d] 不存在", type));
+    }
 }
--- main4-order/main4-order-default/src/main/java/com/shigu/main4/order/model/impl/ItemOrderImpl.java
+++ main4-order/main4-order-default/src/main/java/com/shigu/main4/order/model/impl/ItemOrderImpl.java
@@ -5,11 +5,9 @@ import com.opentae.data.mall.beans.ItemOrderPackage;
 import com.opentae.data.mall.beans.ItemOrderService;
 import com.opentae.data.mall.beans.ItemOrderSub;
 import com.opentae.data.mall.examples.ItemOrderSubExample;
-import com.opentae.data.mall.interfaces.ItemOrderLogisticsMapper;
-import com.opentae.data.mall.interfaces.ItemOrderPackageMapper;
-import com.opentae.data.mall.interfaces.ItemOrderServiceMapper;
-import com.opentae.data.mall.interfaces.ItemOrderSubMapper;
+import com.opentae.data.mall.interfaces.*;
 import com.shigu.main4.common.util.BeanMapper;
+import com.shigu.main4.order.enums.OrderType;
 import com.shigu.main4.order.enums.PayType;
 import com.shigu.main4.order.model.ItemOrder;
 import com.shigu.main4.order.services.OrderConstantService;
@@ -17,9 +15,11 @@ import com.shigu.main4.order.vo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 商品订单
@@ -42,6 +42,12 @@ public class ItemOrderImpl implements ItemOrder{
     private ItemOrderPackageMapper itemOrderPackageMapper;
 
     @Autowired
+    private ItemOrderMapper itemOrderMapper;
+
+    @Autowired
+    private ItemOrderLogisticsMapper itemOrderLogisticsMapper;
+
+    @Autowired
     private OrderConstantService orderConstantService;
 
     /**
@@ -55,26 +61,49 @@ public class ItemOrderImpl implements ItemOrder{
 
     @Override
     public List<LogisticsVO> selLogisticses() {
-        return null;
+        ItemOrderLogistics logistics = new ItemOrderLogistics();
+        logistics.setOid(oid);
+        List<ItemOrderLogistics> select = itemOrderLogisticsMapper.select(logistics);
+        Map<Long, ItemOrderLogistics> logisticsMap = BeanMapper.list2Map(select, "id", Long.class);
+
+        List<LogisticsVO> logisticsVOS = BeanMapper.mapList(select, LogisticsVO.class);
+        for (LogisticsVO logisticsVO : logisticsVOS) {
+            ItemOrderLogistics orderLogistics = logisticsMap.get(logisticsVO.getId());
+
+        }
+        return logisticsVOS;
     }
 
     @Override
     public ItemOrderVO orderInfo() {
-        return null;
+        com.opentae.data.mall.beans.ItemOrder order = itemOrderMapper.selectByPrimaryKey(oid);
+        ItemOrderVO orderVO = BeanMapper.map(order, ItemOrderVO.class);
+        orderVO.setType(OrderType.typeOf(order.getType()));
+        orderVO.setOrderId(order.getOid());
+        return orderVO;
     }
 
     @Override
     public List<SubItemOrderVO> subOrdersInfo() {
-        return null;
+        ItemOrderSub sub = new ItemOrderSub();
+        sub.setOid(oid);
+        List<ItemOrderSub> select = itemOrderSubMapper.select(sub);
+        Map<Long, ItemOrderSub> subOrderMap = BeanMapper.list2Map(select, "soid", Long.class);
+        List<SubItemOrderVO> vos = BeanMapper.mapList(select, SubItemOrderVO.class);
+        for (SubItemOrderVO vo : vos) {
+
+        }
+        return vos;
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public Long addLogistics(List<Long> soids,LogisticsVO logistics) {
         ItemOrderLogistics orderLogistics = BeanMapper.map(logistics, ItemOrderLogistics.class);
         itemOrderLogisticsMapper.insertSelective(orderLogistics);
 
-        // TODO：重新计算订单总额
-        // do something...
+        // 重新计算订单总额
+        recountTotalOrderAmount(selSender().getSenderId());
 
         // 子订单设置物流关联
         if (soids != null && !soids.isEmpty()) {
@@ -89,13 +118,16 @@ public class ItemOrderImpl implements ItemOrder{
 
     @Override
     public void modifyLogistics(Long id, LogisticsVO logistics) {
-
+        logistics.setId(id);
+        ItemOrderLogistics orderLogistics = BeanMapper.map(logistics, ItemOrderLogistics.class);
+        itemOrderLogisticsMapper.updateByPrimaryKeySelective(orderLogistics);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void addPackage(Long metarialId,Integer num) {
-        Long sender = 0L;// TODO: UNKNOWN sender
-        MetarialVO metarialVO = orderConstantService.selMetarialById(sender, metarialId);
+        Long senderId = selSender().getSenderId();
+        MetarialVO metarialVO = orderConstantService.selMetarialById(senderId, metarialId);
         ItemOrderPackage orderPackage = BeanMapper.map(metarialVO, ItemOrderPackage.class);
         orderPackage.setNum(num);
         orderPackage.setMoney(metarialVO.getPrice());
@@ -104,7 +136,7 @@ public class ItemOrderImpl implements ItemOrder{
 
         itemOrderPackageMapper.insertSelective(orderPackage);
 
-        // TODO: 重新计算订单总额 ？
+        recountTotalOrderAmount(senderId);
     }
 
     @Override
@@ -113,16 +145,50 @@ public class ItemOrderImpl implements ItemOrder{
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void addService(Long serviceId) {
         // 记录订单服务信息
-        Long sender = 0L;// TODO: UNKNOWN sender
-        ServiceVO serviceVO = orderConstantService.selServiceById(sender, serviceId);
+        Long senderId = selSender().getSenderId();
+        ServiceVO serviceVO = orderConstantService.selServiceById(senderId, serviceId);
         ItemOrderService itemOrderService = BeanMapper.map(serviceVO, ItemOrderService.class);
         itemOrderService.setMoney(serviceVO.getPrice());
         itemOrderService.setOid(oid);
         itemOrderServiceMapper.insertSelective(itemOrderService);
 
-        // TODO:重新计算订单总额
+        // 重新计算订单总额
+        recountTotalOrderAmount(senderId);
+    }
+
+    /**
+     * TODO:重新计算订单总额
+     * @param senderId 发货机构ID
+     */
+    private void recountTotalOrderAmount(Long senderId) {
+        // 计算订单总额 = 子单总额 + 物流总额 + 发货服务总额 + 包材总额
+        Long total = 0L;
+
+        // 子单总额
+        for (SubItemOrderVO subItemOrderVO : subOrdersInfo()) {
+//            total += subItemOrderVO.getProduct().getPrice();
+        }
+        // 物流总额
+        for (LogisticsVO logisticsVO : selLogisticses()) {
+            total += logisticsVO.getMoney();
+        }
+
+        // 发货服务总额
+        for (ServiceVO serviceVO : orderConstantService.selServices(senderId)) {
+            total += serviceVO.getPrice();
+        }
+
+        for (MetarialVO metarialVO : orderConstantService.selMetarials(senderId)) {
+            total += metarialVO.getPrice();
+        }
+
+        com.opentae.data.mall.beans.ItemOrder order = new com.opentae.data.mall.beans.ItemOrder();
+        order.setOid(oid);
+        order.setTotalFee(total);
+        itemOrderMapper.updateByPrimaryKeySelective(order);
     }
 
     @Override
@@ -141,11 +207,17 @@ public class ItemOrderImpl implements ItemOrder{
     }
 
     @Override
+    public SenderVO selSender() {
+        return null;
+    }
+
+    @Override
     public PayApplyVO payApply(PayType payType) {
         return null;
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void addSubOrder(List<SubOrderVO> subOrders) {
         List<ItemOrderSub> subs = BeanMapper.mapList(subOrders, ItemOrderSub.class);
         for (ItemOrderSub sub : subs) {
@@ -159,7 +231,7 @@ public class ItemOrderImpl implements ItemOrder{
         }
         itemOrderSubMapper.insertListNoId(subs);
 
-        // TODO：重新计算订单总额
+        recountTotalOrderAmount(selSender().getSenderId());
     }
 
     @Override
