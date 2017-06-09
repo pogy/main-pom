584dc8e9936af308160d30c08fcdb923e8e7a7ea
bugzy <xiaochenyoo@sina.com>
2017-06-09 16:24:41 +0800

    MAIN_SITE4-306 按服务ID获取服务信息，实现com.shigu.main4.order.services.OrderConstantService#selServiceById

--- main4-order/main4-order-api/src/main/java/com/shigu/main4/order/vo/ProductVO.java
+++ main4-order/main4-order-api/src/main/java/com/shigu/main4/order/vo/ProductVO.java
@@ -5,4 +5,14 @@ package com.shigu.main4.order.vo;
  * Created by zhaohongbo on 17/6/9.
  */
 public class ProductVO {
+
+    private Long price;
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
 }
