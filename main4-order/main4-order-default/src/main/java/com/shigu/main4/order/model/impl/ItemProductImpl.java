c841b564939b8398045aa2bf3ee63bd9b22b2366
bugzy <xiaochenyoo@sina.com>
2017-06-13 14:20:17 +0800

    MAIN_SITE4-328 修改价格，实现com.shigu.main4.order.model.ItemProduct#modifyPrice

--- main4-order/main4-order-default/src/main/java/com/shigu/main4/order/model/impl/ItemProductImpl.java
+++ main4-order/main4-order-default/src/main/java/com/shigu/main4/order/model/impl/ItemProductImpl.java
@@ -1,7 +1,9 @@
 package com.shigu.main4.order.model.impl;
 
+import com.opentae.data.mall.interfaces.ItemProductMapper;
 import com.shigu.main4.order.model.ItemProduct;
 import com.shigu.main4.order.vo.ItemSkuVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
@@ -16,6 +18,9 @@ import java.util.List;
 public class ItemProductImpl implements ItemProduct{
 
     private Long pid;
+
+    @Autowired
+    private ItemProductMapper itemProductMapper;
 
     public ItemProductImpl(Long goodsId,String color,String size) {
     }
@@ -32,7 +37,10 @@ public class ItemProductImpl implements ItemProduct{
 
     @Override
     public void modifyPrice(Long price) {
-
+        com.opentae.data.mall.beans.ItemProduct product = new com.opentae.data.mall.beans.ItemProduct();
+        product.setPrice(price);
+        product.setPid(pid);
+        itemProductMapper.updateByPrimaryKeySelective(product);
     }
 
     @Override
