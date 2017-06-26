1c3690619294594503c52a41e1ace072d5545eef
bugzy <xiaochenyoo@sina.com>
2017-06-26 19:22:27 +0800

    商品库正常popular

--- web-server/src/main/java/com/shigu/search/actions/GoodsSearchAction.java
+++ web-server/src/main/java/com/shigu/search/actions/GoodsSearchAction.java
@@ -239,7 +239,7 @@ public class GoodsSearchAction {
             orderBy = SearchOrderBy.PRICEUP;
         } else if (bo.getSort().equals("price-desc")) {
             orderBy = SearchOrderBy.PRICEDOWN;
-        } else if (bo.getSort().equals("rq")) {
+        } else if (bo.getSort().equals("popular")) {
             orderBy = SearchOrderBy.POPULAR;
         } else {
             orderBy = SearchOrderBy.GOODS_COMMON;
