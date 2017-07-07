a9a42082e12750b85bf1925411d3fc839b21242d
王浩翔 <whx139@qq.com>
2017-07-07 12:35:50 +0800

    bugfix 修复com.shigu.main4.item.services.ItemSearchService#searchItemByIds中SearchItem价格显示问题

--- main4-store/main4-store-default/src/main/java/com/shigu/main4/storeservices/impl/ShopForCdnServiceImpl.java
+++ main4-store/main4-store-default/src/main/java/com/shigu/main4/storeservices/impl/ShopForCdnServiceImpl.java
@@ -531,7 +531,8 @@ public class ShopForCdnServiceImpl extends ShopServiceImpl implements ShopForCdn
             for (OpenItemVo itemVo : BeanMapper.getFieldList(result.getItems(), "fields", OpenItemVo.class)) {
                 ItemShowBlock itemShowBlock = BeanMapper.map(itemVo,ItemShowBlock.class);
                 itemShowBlock.setImgUrl(itemVo.getPicUrl());
-                itemShowBlock.setPrice((itemVo.getPiPrice()/100)+"."+(itemVo.getPiPrice()%100));
+                long digital = itemVo.getPiPrice()%100;
+                itemShowBlock.setPrice((itemVo.getPiPrice()/100) + "." + (digital/10) + digital%10);
                 itemShowBlock.setItemId(itemVo.getGoodsId());
                 itemShowBlock.setWebSite(webSite);
                 itemShowBlock.setShopId(itemVo.getStoreId());
