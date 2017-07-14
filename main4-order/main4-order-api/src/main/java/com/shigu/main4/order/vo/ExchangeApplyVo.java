ced9aa93a522ec85b39aec860761fc921a373a2b
gubest <gubest_1@126.com>
2017-07-14 17:46:55 +0800

    淘宝订单

---
+++ main4-order/main4-order-api/src/main/java/com/shigu/main4/order/vo/ExchangeApplyVo.java
@@ -0,0 +1,17 @@
+package com.shigu.main4.order.vo;
+
+import java.io.Serializable;
+
+/**
+ * @类编号
+ * @类名称：ExchangeApplyVo
+ * @文件路径：com.shigu.main4.order.vo.ExchangeApplyVo
+ * @内容摘要：
+ * @编码作者：gzy
+ * @创建日期：2017/7/14 17:26
+ * @version: main-pom
+ * @since: main-pom
+ * @commonents:
+ */
+public class ExchangeApplyVo implements Serializable{
+}
