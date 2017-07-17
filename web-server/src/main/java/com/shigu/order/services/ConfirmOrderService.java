17c7ce9aecdafdab42063d1ebbe9506d0baed08f
raymond <raymond323@126.com>
2017-07-17 19:05:11 +0800

    看鞋分站数据表创建

--- web-server/src/main/java/com/shigu/order/services/ConfirmOrderService.java
+++ web-server/src/main/java/com/shigu/order/services/ConfirmOrderService.java
@@ -39,10 +39,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 确认订单
