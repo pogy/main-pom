dffe80b08f50df66dd132a5d354b8de7b09f02d3
raymond <raymond323@126.com>
2017-07-18 16:07:06 +0800

    看鞋分站数据表创建

--- web-server/src/main/java/com/shigu/order/actions/ConfirmOrderAction.java
+++ web-server/src/main/java/com/shigu/order/actions/ConfirmOrderAction.java
@@ -110,6 +110,7 @@ public class ConfirmOrderAction {
         model.addAttribute("serviceRulers", JSON.toJSONString(confirmOrderService.serviceRulePack(vos, bo.getSenderId())));
         model.addAttribute("goodsOrders", vos);
         model.addAttribute("webSite", "hz");//站点
+        model.addAttribute("code", bo.getCode());//站点
         return "trade/confirmOrder";
     }
 
