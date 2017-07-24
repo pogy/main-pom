b2c898361496515d1e35510273ac657ba1ce6d2e
raymond <raymond323@126.com>
2017-07-24 17:18:06 +0800

    OSS接口能力扩展

--- main4-tools/src/main/java/com/shigu/main4/tools/OssFile.java
+++ main4-tools/src/main/java/com/shigu/main4/tools/OssFile.java
@@ -1,14 +1,25 @@
 package com.shigu.main4.tools;
 
 import java.util.Date;
+import java.util.List;
 
 /** Oss文件属性
  * Created by Administrator on 2017/7/24.
  */
 public class OssFile {
+    private int type;//0表示文件；1表示目录
     private String name;
     private Date lastModified;
     private long size;
+    private List<Long> relateedList;//关联的商品id
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType( int type ) {
+        this.type = type;
+    }
 
     public String getName() {
         return name;
@@ -32,5 +43,13 @@ public class OssFile {
 
     public void setSize( long size ) {
         this.size = size;
+    }
+
+    public List<Long> getRelateedList() {
+        return relateedList;
+    }
+
+    public void setRelateedList( List<Long> relateedList ) {
+        this.relateedList = relateedList;
     }
 }
