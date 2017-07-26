659ff49155c334512f0974b9ebba2abb0904eceb
lyszhen3 <947206869@qq.com>
2017-07-26 10:42:48 +0800

    码隆图搜测试类小修改

--- main4-order/main4-order-api/src/main/java/com/shigu/main4/order/enums/ReturnGoodsStatusEnum.java
+++ main4-order/main4-order-api/src/main/java/com/shigu/main4/order/enums/ReturnGoodsStatusEnum.java
@@ -6,10 +6,11 @@ package com.shigu.main4.order.enums;
 public enum ReturnGoodsStatusEnum {
     DISPOSE_RETRUN_GOODS(0),//买家申请退款 对应页面"等待处理退货申请"
     AGREE_PROCESS(1),//同意申请,            对应页面"买家退货-平台同意申请"
-    EXPRESS_SUBMIT(2),//已提交快递,         对应页面"买家退货-退货物理信息提交"
-    RECEIVED(3),//到货,                      对应页面"买家退货-货物收到"
-    WAIT_AFTER_SALE(4),//等待售后处理       对应页面"买家退货-货物收到"
-    RETURN_ENT(5),//退货完成                 对应页面"退货退款完成"
+    REFUSE_PROCESS(2),//拒绝申请
+    EXPRESS_SUBMIT(3),//已提交快递,         对应页面"买家退货-退货物理信息提交"
+    REFUND_MONEY_CHANGED(4),//退款金额已修改 对应 ”买家退货-退款金额已修改“
+    WAIT_AFTER_SALE(5),//等待售后处理       对应页面"买家退货-货物收到"
+    RETURN_ENT(6),//退货完成                 对应页面"退货退款完成"
     ;
     public final int returnGoodsStatus;
 
