38a85a8bf3cf11389d4a24229303075663b902fb
zhaochuanyin <81682024@qq.com>
2017-07-26 17:13:10 +0800

    订单的服务

---
+++ main4-order/main4-order-api/src/main/java/com/shigu/main4/order/vo/OrderServiceVO.java
@@ -0,0 +1,68 @@
+package com.shigu.main4.order.vo;
+
+/**
+ * 订单服务信息
+ * Created by zhaohongbo on 17/7/26.
+ */
+public class OrderServiceVO {
+    /**
+     * 付总金额
+     */
+    private Long money;
+    /**
+     * 数量
+     */
+    private Integer num;
+    /**
+     * 订单ID
+     */
+    private Long oid;
+    /**
+     * 服务名
+     */
+    private String name;
+    /**
+     * 退金额,如果有退的话
+     */
+    private Long refundMoney;
+
+    public Long getMoney() {
+        return money;
+    }
+
+    public void setMoney(Long money) {
+        this.money = money;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public Long getOid() {
+        return oid;
+    }
+
+    public void setOid(Long oid) {
+        this.oid = oid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getRefundMoney() {
+        return refundMoney;
+    }
+
+    public void setRefundMoney(Long refundMoney) {
+        this.refundMoney = refundMoney;
+    }
+}
