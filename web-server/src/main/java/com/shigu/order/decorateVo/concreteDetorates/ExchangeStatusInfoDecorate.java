c55e78653d0f674ee256c248e33281409ede5574
lyszhen3 <947206869@qq.com>
2017-07-27 13:34:07 +0800

    提交

---
+++ web-server/src/main/java/com/shigu/order/decorateVo/concreteDetorates/ExchangeStatusInfoDecorate.java
@@ -0,0 +1,79 @@
+package com.shigu.order.decorateVo.concreteDetorates;
+
+import com.shigu.main4.order.servicevo.AfterSaleStatusVO;
+import com.shigu.order.decorateVo.AbstractRefundVo;
+import com.shigu.order.decorateVo.RefundVoDecorate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by pc on 2017-07-27.
+ *
+ * @author pc
+ * @version 3.0.0-SNAPSHOT
+ * @description
+ * @since 3.0.0-SNAPSHOT
+ */
+public class ExchangeStatusInfoDecorate extends RefundVoDecorate {
+    private AfterSaleStatusVO afterSaleStatusVO;
+
+    public ExchangeStatusInfoDecorate(AbstractRefundVo vo, AfterSaleStatusVO afterSaleStatusVO) {
+        super(vo);
+        this.afterSaleStatusVO = afterSaleStatusVO;
+    }
+
+
+    public ExchangeStatusInfoDecorate(AbstractRefundVo vo) {
+        super(vo);
+    }
+
+    public void setExchangeStatusInfo() {
+        Map<String, Object> map = new HashMap<>();
+        int exchangeStateNum = 0;
+        int exchangeWaitState = 0;
+        map.put("refundId", afterSaleStatusVO.getSubOrderId());
+        map.put("refuseReason", afterSaleStatusVO.getContent());
+        switch (afterSaleStatusVO.getAfterSaleStatus()) {
+
+            case DISPOSE_RETRUN_GOODS: {
+                exchangeStateNum = 2;
+                exchangeWaitState = 1;
+                break;
+            }
+            case REFUSE_PROCESS: {
+                exchangeStateNum = 2;
+                exchangeWaitState = 4;
+                break;
+            }
+            case EXPRESS_SUBMIT: {
+                exchangeStateNum = 2;
+                exchangeWaitState = 3;
+                break;
+            }
+            case AGREE_PROCESS: {
+                exchangeStateNum = 2;
+                exchangeWaitState = 2;
+                break;
+            }
+            case RETURN_ENT: {
+                exchangeStateNum = 3;
+                break;
+            }
+            default: {
+                break;
+            }
+
+        }
+        map.put("exchangeStateNum", exchangeStateNum);
+        map.put("exchangeWaitState", exchangeWaitState);
+        super.addMap(map);
+
+    }
+
+    @Override
+    public void doAdd() {
+        setExchangeStatusInfo();
+        super.doAdd();
+    }
+}
--- web-server/src/main/java/com/shigu/order/services/AfterSaleShowService.java
+++ web-server/src/main/java/com/shigu/order/services/AfterSaleShowService.java
@@ -10,10 +10,12 @@ import com.shigu.main4.order.vo.ExpressVo;
 import com.shigu.main4.order.vo.ReturnableAddressVO;
 import com.shigu.order.bo.AfterSaleBo;
 import com.shigu.order.decorateVo.*;
+import com.shigu.order.decorateVo.concreteCompents.OrderExchangeVo;
 import com.shigu.order.decorateVo.concreteCompents.OrderRefundVo;
 import com.shigu.order.decorateVo.concreteDetorates.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -31,33 +33,25 @@ public class AfterSaleShowService {
     @Autowired
     private AfterSaleService afterSaleService;
 
-    @Autowired
-    private ItemOrderService itemOrderService;
-    @Autowired
-    private LogisticsService logisticsService;
-
-
     public Long applyReturnOrder(AfterSaleBo bo) throws JsonErrException {
-        Long aLong = afterSaleService.returnGoodsApply(Long.parseLong(bo.getChildOrderId()), bo.getRefundCount(), bo.getRefundMoney()
+        return afterSaleService.returnGoodsApply(Long.parseLong(bo.getChildOrderId()), bo.getRefundCount(), bo.getRefundMoney()
                 , bo.getRefundReason(), bo.getRefundDesc());
-        return aLong;
     }
 
-    public void chooseExpress(String childOrderId, String expressId, String expressCode) {
-
+    public void chooseExpress(String refundId, String expressId, String expressCode) {
+        afterSaleService.chooseExpress(Long.parseLong(refundId), Long.parseLong(expressId), expressCode);
     }
 
-    public Map<String,Object> returnOrChange(String childOrderId) {
+    public Map<String, Object> returnOrChange(String childOrderId) {
         SubAfterSaleSimpleOrderVO subAfterSaleSimpleOrderVO = afterSaleService.subAfterSaleSimpleOrder(Long.parseLong(childOrderId));
         AfterSaleSimpleOrderVO afterSaleSimpleOrderVO = afterSaleService.afterSaleSimpleOrder(Long.parseLong(childOrderId));
 
         AbstractRefundVo vo = new OrderRefundVo();
-        AbstractRefundVo vo1 = new SubSimpleRefundDecorate(vo,subAfterSaleSimpleOrderVO);//子单信息
-        AbstractRefundVo vo2 = new OrderSimpleRefundDecorate(vo1,afterSaleSimpleOrderVO);//主单信息
+        AbstractRefundVo vo1 = new SubSimpleRefundDecorate(vo, subAfterSaleSimpleOrderVO);//子单信息
+        AbstractRefundVo vo2 = new OrderSimpleRefundDecorate(vo1, afterSaleSimpleOrderVO);//主单信息
         vo2.doAdd();
         return vo2.getViewVo();
     }
-
 
 
     public Map<String, Object> refundChildOrder(String childOrderId) {
@@ -65,20 +59,20 @@ public class AfterSaleShowService {
         SubAfterSaleSimpleOrderVO subAfterSaleSimpleOrderVO = afterSaleService.subAfterSaleSimpleOrder(Long.parseLong(childOrderId));
         AfterSaleSimpleOrderVO afterSaleSimpleOrderVO = afterSaleService.afterSaleSimpleOrder(Long.parseLong(childOrderId));
         AbstractRefundVo vo = new OrderRefundVo();
-        AbstractRefundVo vo1 = new SubSimpleRefundDecorate(vo,subAfterSaleSimpleOrderVO);//子单信息
-        AbstractRefundVo vo2 = new OrderSimpleRefundDecorate(vo1,afterSaleSimpleOrderVO);//主单信息
+        AbstractRefundVo vo1 = new SubSimpleRefundDecorate(vo, subAfterSaleSimpleOrderVO);//子单信息
+        AbstractRefundVo vo2 = new OrderSimpleRefundDecorate(vo1, afterSaleSimpleOrderVO);//主单信息
         vo2.doAdd();
         Map<String, Object> viewVo = vo2.getViewVo();
 
-        viewVo.put("refundStateNum",1);//这个是申请退货
+        viewVo.put("refundStateNum", 1);//这个是申请退货
 
         return viewVo;
     }
 
-    public Map<String, Object> refund(String refundIds) {
+    public Map<String, Object> refund(String refundIds, Integer express) {
         Long refundId = Long.parseLong(refundIds);
         ShStatusEnum shStatusEnum = afterSaleService.queryAfterSaleType(refundId);
-        if(shStatusEnum == null||shStatusEnum.shStatus == 2){
+        if (shStatusEnum == null || shStatusEnum.shStatus == 2) {
             return null;
         }
 
@@ -87,64 +81,183 @@ public class AfterSaleShowService {
         AfterSaleInfoVO afterSaleInfoVO = afterSaleService.afterSaleInfo(refundId);
         List<RefundLogVO> rlist = afterSaleService.afterSaleApplication(refundId);
         AbstractRefundVo vo = new OrderRefundVo();
-        AbstractRefundVo vo1 = new OrderSimpleRefundDecorate(vo,afterSaleSimpleOrderVO);//主单信息
-        AbstractRefundVo vo2 = new RefundStatusInfoDecorate(vo1,afterSaleStatusVO);//退款状态信息
-        AbstractRefundVo vo3 = new RefundSimpleInfoDecorate(vo2,afterSaleInfoVO);  //退款信息编号和钱
-        AbstractRefundVo vo4 = new RefundLogDecorate(vo3,rlist);//申请信息
+        AbstractRefundVo vo1 = new OrderSimpleRefundDecorate(vo, afterSaleSimpleOrderVO);//主单信息
+        AbstractRefundVo vo2 = new RefundStatusInfoDecorate(vo1, afterSaleStatusVO);//退款状态信息
+        AbstractRefundVo vo3 = new RefundSimpleInfoDecorate(vo2, afterSaleInfoVO);  //退款信息编号和钱
+        AbstractRefundVo vo4 = new RefundLogDecorate(vo3, rlist);//申请信息
         AbstractRefundVo von = null;
-        switch (afterSaleStatusVO.getAfterSaleStatus()){
-            case RETURN_ENT:{
+        switch (afterSaleStatusVO.getAfterSaleStatus()) {
+            case RETURN_ENT: {
                 //页面4
                 AfterSaleEntVO afterSaleEntVO = afterSaleService.afterEnt(refundId);
-                von = new RefundEndDecorate(vo4,afterSaleEntVO);//退款完成
+                von = new RefundEndDecorate(vo4, afterSaleEntVO);//退款完成
                 break;
             }
-            case AGREE_PROCESS:{
+            case AGREE_PROCESS: {
                 //页面3-1
                 ReturnableAddressVO returnableAddressVO = afterSaleService.retrunGoodsAddress(refundId);
                 List<ExpressVo> expressVos = afterSaleService.selectExpress();
-                AbstractRefundVo vo5 = new ReturnAddressDecorate(vo4,returnableAddressVO);//退货地址
-                von = new RefundExpressDetorate(vo5,expressVos);//快递列表
+                AbstractRefundVo vo5 = new ReturnAddressDecorate(vo4, returnableAddressVO);//退货地址
+                von = new RefundExpressDetorate(vo5, expressVos);//快递列表
                 break;
             }
-            case EXPRESS_SUBMIT:{
+            case EXPRESS_SUBMIT: {
                 //页面3-2
                 ReturnableExpressInfoVO returnableExpressInfoVO = afterSaleService.retrunGoodsExpressInfo(refundId);//退货简要信息
-                von = new RefundExpressInfoDecorate(vo4,returnableExpressInfoVO);
+                von = new RefundExpressInfoDecorate(vo4, returnableExpressInfoVO);
                 break;
             }
-            case REFUSE_PROCESS:{
+            case REFUSE_PROCESS: {
                 //页面2-2
                 break;
             }
-            case REFUSE_MONEY_CHANGED:{
+            case REFUSE_MONEY_CHANGED: {
                 //页面3-4
 
                 break;
             }
-            case DISPOSE_RETRUN_GOODS:{
+            case DISPOSE_RETRUN_GOODS: {
                 //2-1
 
                 break;
             }
-            case REFUND_MONEY_CHANGED:{
+            case REFUND_MONEY_CHANGED: {
                 //3-3
+                break;
+            }
+            default: {
+                break;
+            }
+
+
+        }
+        Map<String,Object> returnmap;
+        if (von == null) {
+          von = vo4;
+        }
+        von.doAdd();
+        returnmap = von.getViewVo();
+        //修改快递特殊处理
+        if(!StringUtils.isEmpty(express) && express == 1
+                && (returnmap.get("refundStateNum")!=null&& (Integer)returnmap.get("refundStateNum")== 3)
+                && (returnmap.get("returnState")!=null && (Integer)returnmap.get("returnState") == 2)
+                ){
+           returnmap.put("express",express);
+           returnmap.replace("refundStateNum",3);
+           returnmap.replace("returnState",1);
+        }
+
+        return returnmap;
+
+
+    }
+
+    public Map<String, Object> exchangeChildOrder(long childOrderId) {
+        SubAfterSaleSimpleOrderVO subAfterSaleSimpleOrderVO = afterSaleService.subAfterSaleSimpleOrder(childOrderId);
+        AfterSaleSimpleOrderVO afterSaleSimpleOrderVO = afterSaleService.afterSaleSimpleOrder(childOrderId);
+        AbstractRefundVo vo = new OrderExchangeVo();
+        AbstractRefundVo vo1 = new SubSimpleRefundDecorate(vo, subAfterSaleSimpleOrderVO);//子单信息
+        AbstractRefundVo vo2 = new OrderSimpleRefundDecorate(vo1, afterSaleSimpleOrderVO);//主单信息
+        vo2.doAdd();
+        Map<String, Object> viewVo = vo2.getViewVo();
+
+        viewVo.put("exchangeStateNum", 1);//这个是申请退货
+
+        return viewVo;
+    }
+
+    public Map<String, Object> exchange(long refundId,Integer express) {
+        ShStatusEnum shStatusEnum = afterSaleService.queryAfterSaleType(refundId);
+        if (shStatusEnum == null || shStatusEnum.shStatus == 1) {
+            return null;
+        }
+        AfterSaleStatusVO afterSaleStatusVO = afterSaleService.afterSaleStatus(refundId);
+        AfterSaleSimpleOrderVO afterSaleSimpleOrderVO = afterSaleService.afterSaleSimpleOrder(afterSaleStatusVO.getSubOrderId());
+        AbstractRefundVo vo = new OrderExchangeVo();
+        AbstractRefundVo vo1 = new OrderSimpleRefundDecorate(vo, afterSaleSimpleOrderVO);//主单信息
+        AfterSaleInfoVO afterSaleInfoVO = afterSaleService.afterSaleInfo(refundId);
+        List<RefundLogVO> rlist = afterSaleService.afterSaleApplication(refundId);
+        AbstractRefundVo vo2 = new ExchangeStatusInfoDecorate(vo1, afterSaleStatusVO);//换货退款状态信息
+        AbstractRefundVo vo3 = new RefundSimpleInfoDecorate(vo2, afterSaleInfoVO);  //退款信息编号和钱
+        AbstractRefundVo vo4 = new RefundLogDecorate(vo3, rlist);//申请信息
+        AbstractRefundVo von = null;
+
+        switch (afterSaleStatusVO.getAfterSaleStatus()) {
+            // 2-1
+            case DISPOSE_RETRUN_GOODS: {
+                break;
+            }
+            //2-4
+            case REFUSE_PROCESS: {
+                break;
+            }
+            //2-3
+            case EXPRESS_SUBMIT: {
+                ReturnableExpressInfoVO returnableExpressInfoVO = afterSaleService.retrunGoodsExpressInfo(refundId);//退货简要信息
+                von = new RefundExpressInfoDecorate(vo4, returnableExpressInfoVO);
+                break;
+            }
+            //2-2
+            case AGREE_PROCESS: {
+                ReturnableAddressVO returnableAddressVO = afterSaleService.retrunGoodsAddress(refundId);
+                List<ExpressVo> expressVos = afterSaleService.selectExpress();
+                AbstractRefundVo vo5 = new ReturnAddressDecorate(vo4, returnableAddressVO);//退货地址
+                von = new RefundExpressDetorate(vo5, expressVos);//快递列表
+                break;
+            }
+            //3
+            case RETURN_ENT: {
+                AfterSaleEntVO afterSaleEntVO = afterSaleService.afterEnt(refundId);
+                von = new ExchangeEndDecorate(vo4, afterSaleEntVO);//退款完成
+                break;
+            }
+            default: {
+                break;
+            }
+
+
+        }
+         Map<String,Object> returnmap;
+        if (von == null) {
+            von = vo4;
+        }
+        von.doAdd();
+        returnmap = von.getViewVo();
+        //修改快递特殊处理
+        if(!StringUtils.isEmpty(express) && express == 1
+                && (returnmap.get("exchangeStateNum")!=null&& (Integer)returnmap.get("exchangeStateNum")== 2)
+                && (returnmap.get("exchangeWaitState")!=null && (Integer)returnmap.get("exchangeWaitState") == 2)
+                ){
+           returnmap.put("express",express);
+           returnmap.replace("refundStateNum",2);
+           returnmap.replace("returnState",1);
+        }
+        return returnmap;
+    }
+
+    public void modifyExpress(String refundId, String expressId, String expressCode) {
+        afterSaleService.modifyExpress(Long.parseLong(refundId),Long.parseLong(expressId),expressCode);
+    }
+
+    public Long exchangeApply(AfterSaleBo bo) {
+        return afterSaleService.exchangeApply(Long.parseLong(bo.getChildOrderId()),bo.getRefundReason(),bo.getRefundDesc());
+    }
+
+    public void agreeRefunMoney(long refundId, Integer agreeState) {
+        boolean isAgree = false;
+        switch (agreeState){
+            case 1:{
+                isAgree = true;
+                break;
+            }
+            case 2:{
+                isAgree = false;
                 break;
             }
             default:{
                 break;
             }
-
-
         }
-        if(von == null){
-            vo4.doAdd();
-           return vo4.getViewVo();
-        }else{
-            von.doAdd();
-            return von.getViewVo();
-        }
-
-
+        afterSaleService.agreeOrRejectRefundPrice(refundId,isAgree);
     }
 }
