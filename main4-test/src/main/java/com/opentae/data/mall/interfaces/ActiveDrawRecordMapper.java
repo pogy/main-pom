9a0d5e878d4204c28cee2bb74fbd91c692f2c4ac
王浩翔 <whx139@qq.com>
2017-08-02 14:18:15 +0800

    main4-test子模块方法卸载

--- main4-test/src/main/java/com/opentae/data/mall/interfaces/ActiveDrawRecordMapper.java
+++ main4-test/src/main/java/com/opentae/data/mall/interfaces/ActiveDrawRecordMapper.java
@@ -18,15 +18,4 @@ import java.util.List;
 @Lazy(true)
 public interface ActiveDrawRecordMapper extends Mapper<ActiveDrawRecord> {
 
-    /** 用户抽奖数据 */
-    List<ActiveDrawRecord> selDrawRecordList(@Param("pemId") Long pemId, @Param("userId") Long userId,
-                                             @Param("type") String type, @Param("ward") String ward,
-                                             @Param("userNick") String userNick, @Param("startRows")Integer startRows,
-                                             @Param("endRows")Integer endRows);
-
-    /** 用户抽奖数据数 */
-    int selDrawRecordCount(@Param("pemId") Long pemId, @Param("userId") Long userId,
-                           @Param("ward") String ward, @Param("userNick") String userNick);
-
-
 }
