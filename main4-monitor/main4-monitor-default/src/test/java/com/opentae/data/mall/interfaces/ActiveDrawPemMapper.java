53e4d2a2159c6a10603f46b91bcefb4ad05550e8
王浩翔 <whx139@qq.com>
2017-08-02 13:48:13 +0800

    main4-monitor-default子模块方法卸载及测试类dao添加

--- main4-monitor/main4-monitor-default/src/test/java/com/opentae/data/mall/interfaces/ActiveDrawPemMapper.java
+++ main4-monitor/main4-monitor-default/src/test/java/com/opentae/data/mall/interfaces/ActiveDrawPemMapper.java
@@ -2,20 +2,16 @@ package com.opentae.data.mall.interfaces;
 
 import com.opentae.core.mybatis.config.MyBatisRepository;
 import com.opentae.core.mybatis.mapper.Mapper;
-import com.opentae.data.mall.beans.ShiguShop;
-import com.opentae.data.mall.beans.ShopNumAndMarket;
-import org.apache.ibatis.annotations.Param;
+import com.opentae.data.mall.beans.ActiveDrawPem;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
-
-import java.util.List;
 
 /**
  * 
  * Created by exampleCreater.
  */
-@MyBatisRepository("tae_mall_shiguShopMapper")
+@MyBatisRepository("tae_mall_activeDrawPemMapper")
 @Scope("singleton")
 @Lazy(true)
-public interface ShiguShopMapper extends Mapper<ShiguShop> {
+public interface ActiveDrawPemMapper extends Mapper<ActiveDrawPem> {
 }
