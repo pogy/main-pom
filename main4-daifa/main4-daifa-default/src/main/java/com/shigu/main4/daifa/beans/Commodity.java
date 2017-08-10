c9877ae8a9c6d523e38c895982ea66d5f26d06a3
gubest <gubest_1@126.com>
2017-08-10 15:46:13 +0800

    快递信息调用

---
+++ main4-daifa/main4-daifa-default/src/main/java/com/shigu/main4/daifa/beans/Commodity.java
@@ -0,0 +1,13 @@
+package com.shigu.main4.daifa.beans;
+
+public class Commodity {
+	private String GoodsName;//商品名称
+
+	public String getGoodsName() {
+		return GoodsName;
+	}
+
+	public void setGoodsName(String goodsName) {
+		GoodsName = goodsName;
+	}
+}
