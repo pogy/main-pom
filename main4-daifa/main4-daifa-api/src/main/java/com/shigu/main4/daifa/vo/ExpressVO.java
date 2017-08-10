e6bb3f25a1c40d08dab01c4ed25fa443fdbcd90d
gubest <gubest_1@126.com>
2017-08-10 09:12:34 +0800

    快递信息调用的数据持久化

--- main4-daifa/main4-daifa-api/src/main/java/com/shigu/main4/daifa/vo/ExpressVO.java
+++ main4-daifa/main4-daifa-api/src/main/java/com/shigu/main4/daifa/vo/ExpressVO.java
@@ -12,4 +12,50 @@ package com.shigu.main4.daifa.vo;
  * @commonents:
  */
 public class ExpressVO {
+
+    private Long tid;//代发交易ID
+    private Long expressId;//快递ID
+    private String expressCode;//快递单号
+    private String markDestination;//三段码
+    private String packageName;//集包地
+
+    public Long getTid () {
+        return tid;
+    }
+
+    public void setTid (Long tid) {
+        this.tid = tid;
+    }
+
+    public Long getExpressId () {
+        return expressId;
+    }
+
+    public void setExpressId (Long expressId) {
+        this.expressId = expressId;
+    }
+
+    public String getExpressCode () {
+        return expressCode;
+    }
+
+    public void setExpressCode (String expressCode) {
+        this.expressCode = expressCode;
+    }
+
+    public String getMarkDestination () {
+        return markDestination;
+    }
+
+    public void setMarkDestination (String markDestination) {
+        this.markDestination = markDestination;
+    }
+
+    public String getPackageName () {
+        return packageName;
+    }
+
+    public void setPackageName (String packageName) {
+        this.packageName = packageName;
+    }
 }
