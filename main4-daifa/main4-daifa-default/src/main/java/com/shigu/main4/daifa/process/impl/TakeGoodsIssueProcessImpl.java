49cbe4ada7dd574831ae1b2ac9e4c7f7252723f0
lyszhen3 <947206869@qq.com>
2017-08-11 14:13:53 +0800

    打印条码

--- main4-daifa/main4-daifa-default/src/main/java/com/shigu/main4/daifa/process/impl/TakeGoodsIssueProcessImpl.java
+++ main4-daifa/main4-daifa-default/src/main/java/com/shigu/main4/daifa/process/impl/TakeGoodsIssueProcessImpl.java
@@ -1,17 +1,27 @@
-package com.shigu.main4.daifa.process;
+package com.shigu.main4.daifa.process.impl;
 
+import com.opentae.core.mybatis.example.MultipleExample;
+import com.opentae.core.mybatis.example.MultipleExampleBuilder;
+import com.opentae.core.mybatis.mapper.MultipleMapper;
 import com.opentae.core.mybatis.utils.FieldUtil;
 import com.opentae.data.daifa.beans.DaifaGgoodsTasks;
+import com.opentae.data.daifa.examples.DaifaGgoodsExample;
 import com.opentae.data.daifa.examples.DaifaGgoodsTasksExample;
+import com.opentae.data.daifa.examples.DaifaOrderExample;
+import com.opentae.data.daifa.examples.DaifaTradeExample;
 import com.opentae.data.daifa.interfaces.DaifaGgoodsTasksMapper;
+import com.shigu.main4.common.util.DateUtil;
+import com.shigu.main4.daifa.beans.GgoodsForPrint;
 import com.shigu.main4.daifa.exceptions.DaifaException;
 import com.shigu.main4.daifa.model.CargoManModel;
+import com.shigu.main4.daifa.process.TakeGoodsIssueProcess;
 import com.shigu.main4.daifa.vo.PrintTagVO;
 import com.shigu.main4.tools.SpringBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -26,13 +36,18 @@ import java.util.List;
  * @commonents:
  */
 @Service
-public class TakeGoodsIssueProcessImpl implements TakeGoodsIssueProcess{
+public class TakeGoodsIssueProcessImpl implements TakeGoodsIssueProcess {
 
-
+    private final static Integer EZINT = 7; //截取长度
     private DaifaGgoodsTasksMapper daifaGgoodsTasksMapper;
     @Autowired
     public void setDaifaGgoodsTasksMapper(DaifaGgoodsTasksMapper daifaGgoodsTasksMapper) {
         this.daifaGgoodsTasksMapper = daifaGgoodsTasksMapper;
+    }
+    private MultipleMapper multipleMapper;
+    @Autowired
+    public void setMultipleMapper(MultipleMapper multipleMapper) {
+        this.multipleMapper = multipleMapper;
     }
 
     @Override
@@ -97,10 +112,56 @@ public class TakeGoodsIssueProcessImpl implements TakeGoodsIssueProcess{
 
     @Override
     public List<PrintTagVO> printTags (List<Long> issueIds) {
-        
+        String nowDate= DateUtil.dateToString(new Date(),DateUtil.patternB);
+        //先根据ids查询出数据
+        DaifaGgoodsExample dgex = new DaifaGgoodsExample();
+        DaifaOrderExample doex = new DaifaOrderExample();
+        DaifaTradeExample dtex = new DaifaTradeExample();
+
+        MultipleExample multipleExample = MultipleExampleBuilder.from(dgex).join(dtex)
+                .on(dgex.createCriteria().andTakeGoodsIdIn(issueIds)
+                        .equalTo(DaifaGgoodsExample.dfTradeId, DaifaTradeExample.dfTradeId)).join(doex)
+                .on(dgex.createCriteria().equalTo(DaifaGgoodsExample.dfOrderId, DaifaOrderExample.dfOrderId)).build();
+        List<GgoodsForPrint> ggoodsForPrints = multipleMapper
+                .selectFieldsByMultipleExample(multipleExample, GgoodsForPrint.class);
+
+        List<PrintTagVO> pvos = new ArrayList<>();
+
+//
+//        CountTransExample ctex=new CountTransExample();
+//        ctex.setOrderByClause("id desc");
+//        ctex.setStartIndex(0);
+//        ctex.setEndIndex(1);
+//        ctex.createCriteria().andCreateDateEqualTo(nowDate);
+//        //查询当天有无操作过批次
+//        List<CountTrans> ctlist=countTransMapper.selectByConditionList(ctex);
+//
+//        int startindex=0;
+//        if(ctlist.size()>0){
+//            startindex=ctlist.get(0).getFinalNumber().intValue();
+//        }
 
 
-        return null;
+        ggoodsForPrints.forEach(ggoodsForPrint -> {
+            PrintTagVO vo  = new PrintTagVO();
+            vo.setOrderSort(ggoodsForPrint.getBarCodeKeyNum());
+            vo.setSpecialStr(ggoodsForPrint.getBarCodeKey());
+            vo.setPackages(ggoodsForPrint.getGoodsNum());
+            String barcode = ggoodsForPrint.getDfOrderId().toString() + ggoodsForPrint.getDfTradeId().toString()
+                    .substring(ggoodsForPrint.getDfTradeId().toString().length() - EZINT);//计算长度
+            vo.setBarCode(barcode);
+            vo.setpAndBarCode((int)Double.parseDouble(ggoodsForPrint.getSinglePiPrice())+"N"+barcode);
+            vo.setBuyerNick(ggoodsForPrint.getBuyerNick());
+            vo.setReceiverName(ggoodsForPrint.getRecieverName());
+
+
+
+
+
+        });
+
+
+        return pvos;
     }
 
     @Override
