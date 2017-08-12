9168fd60095a22612b02e1c5f934ba1e814d4baf
lyszhen3 <947206869@qq.com>
2017-08-12 13:18:30 +0800

    打印测试类修改

--- main4-daifa/main4-daifa-default/src/test/java/com/shigu/main4/order/model/impltest/TakeGoodsIssueProcessTest.java
+++ main4-daifa/main4-daifa-default/src/test/java/com/shigu/main4/order/model/impltest/TakeGoodsIssueProcessTest.java
@@ -1,8 +1,10 @@
 package com.shigu.main4.order.model.impltest;
 
+import com.opentae.data.daifa.beans.DaifaGgoods;
 import com.opentae.data.daifa.beans.DaifaGgoodsTasks;
 import com.opentae.data.daifa.beans.DaifaOrder;
 import com.opentae.data.daifa.beans.DaifaTrade;
+import com.opentae.data.daifa.examples.DaifaGgoodsExample;
 import com.opentae.data.daifa.interfaces.*;
 import com.shigu.main4.common.util.BeanMapper;
 import com.shigu.main4.daifa.bo.SubOrderModelBO;
@@ -34,7 +36,7 @@ import static org.junit.Assert.assertNotEquals;
  * @description
  * @since 3.0.0-SNAPSHOT
  */
-public class TakeGoodsIssueProcessTest extends BaseSpringTest{
+public class TakeGoodsIssueProcessTest extends BaseSpringTest {
     @Autowired
     DaifaOrderMapper daifaOrderMapper;
     @Autowired
@@ -53,35 +55,43 @@ public class TakeGoodsIssueProcessTest extends BaseSpringTest{
     @Test
     @Transactional
     public void printAllTags_test() throws DaifaException {
-        DaifaTrade trade=insertTrade(99999L);
-         DaifaGgoodsTasks daifaGgoodsTasks=new DaifaGgoodsTasks();
-        daifaGgoodsTasks.setDfTradeId(99999L);
-        List<DaifaGgoodsTasks> ts=daifaGgoodsTasksMapper.select(daifaGgoodsTasks);
-        assertNotEquals(ts.size(),0);
-        List<Long> tids=new ArrayList<>();
-        Long oid=null;
-        for(DaifaGgoodsTasks t:ts){
-            if(t.getAllocatStatus()==0){
+        DaifaTrade trade = insertTrade(82894793479L);
+        DaifaGgoodsTasks daifaGgoodsTasks = new DaifaGgoodsTasks();
+        daifaGgoodsTasks.setDfTradeId(82894793479L);
+        List<DaifaGgoodsTasks> ts = daifaGgoodsTasksMapper.select(daifaGgoodsTasks);
+        assertNotEquals(ts.size(), 0);
+        List<Long> tids = new ArrayList<>();
+        Long oid = null;
+        for (DaifaGgoodsTasks t : ts) {
+            if (t.getAllocatStatus() == 0) {
                 tids.add(t.getTasksId());
-                oid=t.getDfOrderId();
+                oid = t.getDfOrderId();
             }
         }
-        boolean b=false;
+        boolean b = false;
 
         CargoManModel bean = SpringBeanFactory.getBean(CargoManModel.class, 2L);
         bean.takeToMe(tids);
 
+
+
         List<PrintTagVO> printTagVOS = takeGoodsIssueProcess.printAllTags(999999999L);
+        assertNotEquals(printTagVOS.size(),0);
+        DaifaGgoodsExample daifaGgoodsExample = new DaifaGgoodsExample();
+        daifaGgoodsExample.createCriteria().andDfTradeIdEqualTo(82894793479L);
+        List<DaifaGgoods> ggoods = daifaGgoodsMapper.selectByExample(daifaGgoodsExample);
+        ggoods.forEach(ggood->{assertEquals(ggood.getPrintBarcodeStatus(),new Integer(2));
+            System.out.println("---------------批次号----"+ggood.getPrintBatch());
+        });
 
-        System.out.println(printTagVOS.get(0).getBarCode());
-
+        System.out.println("-----------打打-----"+printTagVOS.get(0).getpAndBarCode());
 
 
     }
 
 
-    private DaifaTrade insertTrade(Long tid){
-        DaifaTrade t=new DaifaTrade();
+    private DaifaTrade insertTrade(Long tid) {
+        DaifaTrade t = new DaifaTrade();
         t.setBarCodeKey("A02");
         t.setBuyerId(100012567L);
         t.setBuyerName("zf");
@@ -122,13 +132,13 @@ public class TakeGoodsIssueProcessTest extends BaseSpringTest{
         t.setDfTradeId(tid);
         daifaTradeMapper.insertSelective(t);
 
-        insert(tid,"1-2");
-        insert(tid,"2-2");
+        insert(tid, "1-2");
+        insert(tid, "2-2");
         return t;
     }
 
-     private Long insert(Long tid,String code){
-        SubOrderModelBO bo=new SubOrderModelBO();
+    private Long insert(Long tid, String code) {
+        SubOrderModelBO bo = new SubOrderModelBO();
         bo.setAggrement(1);
         bo.setBarCodeKeyNum(code);
         bo.setCid(10000L);
@@ -159,46 +169,46 @@ public class TakeGoodsIssueProcessTest extends BaseSpringTest{
         bo.setTotalServiceFee("1.00");
         bo.setSellerId(999999999L);
         bo.setWebSite("hz");
-        SubOrderModelImpl impl= SpringBeanFactory.getBean(SubOrderModelImpl.class,bo);
+        SubOrderModelImpl impl = SpringBeanFactory.getBean(SubOrderModelImpl.class, bo);
 
-        DaifaOrder order=daifaOrderMapper.selectByPrimaryKey(impl.getSubOrderId());
-        DaifaGgoodsTasks task=new DaifaGgoodsTasks();
+        DaifaOrder order = daifaOrderMapper.selectByPrimaryKey(impl.getSubOrderId());
+        DaifaGgoodsTasks task = new DaifaGgoodsTasks();
         task.setDfOrderId(impl.getSubOrderId());
-        task=daifaGgoodsTasksMapper.selectOne(task);
-        SubOrderModelBO bo1= BeanMapper.map(order,SubOrderModelBO.class);
-        SubOrderModelBO bo2= BeanMapper.map(task,SubOrderModelBO.class);
-        Field[] fs=SubOrderModelBO.class.getDeclaredFields();
-        for(Field f:fs){
+        task = daifaGgoodsTasksMapper.selectOne(task);
+        SubOrderModelBO bo1 = BeanMapper.map(order, SubOrderModelBO.class);
+        SubOrderModelBO bo2 = BeanMapper.map(task, SubOrderModelBO.class);
+        Field[] fs = SubOrderModelBO.class.getDeclaredFields();
+        for (Field f : fs) {
             f.setAccessible(true);
             try {
-                Object b=f.get(bo);
-                if(!f.getName().equals("sellerId")&&!f.getName().equals("webSite"))assertEquals(b,f.get(bo1));
-                Object b2=f.get(bo2);
-                if(b2!=null)assertEquals(b,b2);
+                Object b = f.get(bo);
+                if (!f.getName().equals("sellerId") && !f.getName().equals("webSite")) assertEquals(b, f.get(bo1));
+                Object b2 = f.get(bo2);
+                if (b2 != null) assertEquals(b, b2);
             } catch (IllegalAccessException e) {
                 System.out.println(f.getName());
                 assertFalse(false);
             }
         }
-        assertEquals(order.getDelistIs(),new Integer(0));
-        assertEquals(order.getAllocatStatus(),new Integer(0));
+        assertEquals(order.getDelistIs(), new Integer(0));
+        assertEquals(order.getAllocatStatus(), new Integer(0));
 
-        assertEquals(task.getDelistIs(),new Integer(0));
-        assertEquals(task.getAllocatStatus(),new Integer(0));
-        assertEquals(task.getTakeGoodsDate(),null);
-        assertEquals(task.getTakeGoodsStatus(),new Integer(0));
-        assertEquals(task.getOperateIs(),new Integer(0));
-        assertEquals(task.getUseStatus(),new Integer(1));
-        assertEquals(task.getDaifaWorker(),null);
-        assertEquals(task.getDaifaWorkerId(),null);
-        assertEquals(task.getAllocatDate(),null);
-        assertEquals(task.getAllocatTime(),null);
-        assertEquals(task.getEndStatus(),new Integer(0));
-        assertEquals(task.getGgoodsCode(),null);
-        assertEquals(task.getPrintBarcodeStatus(),new Integer(1));
-        assertEquals(task.getPrintGoodsStatus(),new Integer(1));
-        assertEquals(task.getPrintBatch(),null);
-        assertEquals(task.getReturnStatus(),new Integer(0));
+        assertEquals(task.getDelistIs(), new Integer(0));
+        assertEquals(task.getAllocatStatus(), new Integer(0));
+        assertEquals(task.getTakeGoodsDate(), null);
+        assertEquals(task.getTakeGoodsStatus(), new Integer(0));
+        assertEquals(task.getOperateIs(), new Integer(0));
+        assertEquals(task.getUseStatus(), new Integer(1));
+        assertEquals(task.getDaifaWorker(), null);
+        assertEquals(task.getDaifaWorkerId(), null);
+        assertEquals(task.getAllocatDate(), null);
+        assertEquals(task.getAllocatTime(), null);
+        assertEquals(task.getEndStatus(), new Integer(0));
+        assertEquals(task.getGgoodsCode(), null);
+        assertEquals(task.getPrintBarcodeStatus(), new Integer(1));
+        assertEquals(task.getPrintGoodsStatus(), new Integer(1));
+        assertEquals(task.getPrintBatch(), null);
+        assertEquals(task.getReturnStatus(), new Integer(0));
         return impl.getSubOrderId();
     }
 
