cf8d59abd8284bc7e8a68dfbac12c4394a33111a
bugzy <xiaochenyoo@sina.com>
2017-08-14 13:21:22 +0800

    fix: ...

---
+++ main4-order/main4-order-default/src/main/java/com/shigu/main4/order/mq/DfMessageListener.java
@@ -0,0 +1,19 @@
+package com.shigu.main4.order.mq;
+
+import com.aliyun.openservices.ons.api.Action;
+import com.aliyun.openservices.ons.api.ConsumeContext;
+import com.aliyun.openservices.ons.api.Message;
+import com.aliyun.openservices.ons.api.MessageListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 代发消息队列接收处理
+ * Created by bugzy on 2017/8/14 0014.
+ */
+@Component
+public class DfMessageListener implements MessageListener {
+    @Override
+    public Action consume(Message message, ConsumeContext consumeContext) {
+        return null;
+    }
+}
