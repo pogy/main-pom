183f52315c56a9fb2b86a7320867072864cc0971
zf <460333383@qq.com>
2017-09-14 14:49:14 +0800

    上程序,先改回来先

--- web-server/src/main/java/com/shigu/order/services/MyOrderService.java
+++ web-server/src/main/java/com/shigu/order/services/MyOrderService.java
@@ -75,8 +75,8 @@ public class MyOrderService {
 
     public ShiguPager<MyOrderVO> selectMyOrderPager(OrderBO bo, Long userId) throws ParseException {
         ItemOrderExample orderExample = new ItemOrderExample();
-        //orderExample.setStartIndex((bo.getPage() - 1) * bo.getPageSize());
-        //orderExample.setEndIndex(bo.getPageSize());
+        orderExample.setStartIndex((bo.getPage() - 1) * bo.getPageSize());
+        orderExample.setEndIndex(bo.getPageSize());
         orderExample.setOrderByClause("item_order.create_time DESC");
         ItemOrderExample.Criteria orderCriteria = orderExample.createCriteria().andUserIdEqualTo(userId).andDisenableEqualTo(false);
 
@@ -130,8 +130,8 @@ public class MyOrderService {
         int orderCount = multipleMapper.countByMultipleExample(multipleExample);
         if (orderCount > 0) {
 
-            multipleExample.setStartIndex((number-1)*size);
-            multipleExample.setEndIndex(size);
+//            multipleExample.setStartIndex((number-1)*size);
+//            multipleExample.setEndIndex(size);
             // 查询数据
             pager.calPages(orderCount, size);
             pager.setContent(multipleMapper.selectFieldsByMultipleExample(multipleExample, MyOrderVO.class));
