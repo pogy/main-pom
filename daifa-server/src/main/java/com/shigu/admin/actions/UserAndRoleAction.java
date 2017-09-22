9dfa7f1eed625ec025b5ef6e270d0889938149f8
gubest <gubest_1@126.com>
2017-09-22 16:40:03 +0800

    系统处理

--- daifa-server/src/main/java/com/shigu/admin/actions/UserAndRoleAction.java
+++ daifa-server/src/main/java/com/shigu/admin/actions/UserAndRoleAction.java
@@ -38,8 +38,6 @@ public class UserAndRoleAction {
 
     @RequestMapping("admin/userAndRole")
     public String userAndRole(UserAndRoleBO bo, Model model) throws ExecutionException, InterruptedException {
-
-
         //Session session = SecurityUtils.getSubject().getSession();
         //  AuthorityUser auth = (AuthorityUser) session.getAttribute(DaifaSessionConfig.DAIFA_SESSION);
         Long sellerId=999999990L;
