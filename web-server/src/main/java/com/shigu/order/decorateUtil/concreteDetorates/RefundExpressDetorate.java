eeb72cc74f6fcb91d13e59b4fee60f0b1f021251
王浩翔 <whx139@qq.com>
2017-09-25 15:29:49 +0800

    原来快递信息字符串写死不匹配

--- web-server/src/main/java/com/shigu/order/decorateUtil/concreteDetorates/RefundExpressDetorate.java
+++ web-server/src/main/java/com/shigu/order/decorateUtil/concreteDetorates/RefundExpressDetorate.java
@@ -1,9 +1,13 @@
 package com.shigu.order.decorateUtil.concreteDetorates;
 
+import com.shigu.main4.order.services.AfterSaleService;
 import com.shigu.main4.order.vo.ExpressVo;
 import com.shigu.order.decorateUtil.AbstractRefundVo;
 import com.shigu.order.decorateUtil.RefundVoDecorate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,9 +21,13 @@ import java.util.Map;
  * @description
  * @since 3.0.0-SNAPSHOT
  */
+@Component
 public class RefundExpressDetorate extends RefundVoDecorate {
+    @Autowired
+    AfterSaleService afterSaleService;
+
     private List<ExpressVo> list;
-    private static String express = "1:申通快递,2:圆通快递,3:韵达快递,4:百世汇通,5:EMS,6:其他";
+    private static String express;
     public RefundExpressDetorate(AbstractRefundVo vo, List<ExpressVo> list){
         super(vo);
         this.list = list;
@@ -42,6 +50,23 @@ public class RefundExpressDetorate extends RefundVoDecorate {
 
         map.put("expressList",builder.toString());
         super.addMap(map);
+    }
+
+    @PostConstruct
+    public void init() {
+        if (express == null) {
+            List<ExpressVo> expressVos = afterSaleService.selectExpress();
+            StringBuffer sb = new StringBuffer();
+            for (ExpressVo expressVo : expressVos) {
+                sb.append(',').append(expressVo.getExpressId()).append(':').append(expressVo.getExpressName());
+            }
+            if (sb.length()>0) {
+                express=sb.substring(1);
+            } else {
+                express = "1:申通快递,2:圆通快递,3:韵达快递,4:百世汇通,5:EMS,6:其他";
+            }
+        }
+        return;
     }
 
     @Override
