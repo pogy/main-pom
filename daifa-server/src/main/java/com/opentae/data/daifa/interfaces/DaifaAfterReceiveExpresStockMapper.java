4702123cf2d0132d4447274a62c011026f5da4e5
zf <460333383@qq.com>
2017-09-25 10:12:27 +0800

    生成错误处理

--- daifa-server/src/main/java/com/opentae/data/daifa/interfaces/DaifaAfterReceiveExpresStockMapper.java
+++ daifa-server/src/main/java/com/opentae/data/daifa/interfaces/DaifaAfterReceiveExpresStockMapper.java
@@ -3,8 +3,11 @@ package com.opentae.data.daifa.interfaces;
 import com.opentae.core.mybatis.config.MyBatisRepository;
 import com.opentae.core.mybatis.mapper.Mapper;
 import com.opentae.data.daifa.beans.DaifaAfterReceiveExpresStock;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
+
+import java.util.List;
 
 /**
  * 
@@ -14,4 +17,13 @@ import org.springframework.context.annotation.Scope;
 @Scope("singleton")
 @Lazy(true)
 public interface DaifaAfterReceiveExpresStockMapper extends Mapper<DaifaAfterReceiveExpresStock> {
+    Integer selectExpressPackageCount(@Param("sendPhone")String sendPhone,
+                                      @Param("postCode")String postCode,
+                                      @Param("status")Integer status);
+
+    List<DaifaAfterReceiveExpresStock> selectExpressPackage(@Param("sendPhone")String sendPhone,
+                                                            @Param("postCode")String postCode,
+                                                            @Param("status")Integer status,
+                                                            @Param("start")Integer start,
+                                                            @Param("end")Integer end);
 }
