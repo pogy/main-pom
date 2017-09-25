6499625729ddf2643a7a23fb73d19a1f5becd769
raymond <raymond323@126.com>
2017-09-25 08:57:10 +0800

    增加一个appUser类

--- main4-sdks/main4-app-sdk/src/main/java/com/openJar/enums/HostEnum.java
+++ main4-sdks/main4-app-sdk/src/main/java/com/openJar/enums/HostEnum.java
@@ -3,7 +3,7 @@ package com.openJar.enums;
 public enum HostEnum {
     //服务器   http://192.168.1.193/app/
 
-    TEST("http://127.0.0.1:8080/app/"),REST("http://");
+    TEST("http://192.168.1.193/app/"),REST("http://");
 
     //本地    http://127.0.0.1:8080/app/
     private String host;
