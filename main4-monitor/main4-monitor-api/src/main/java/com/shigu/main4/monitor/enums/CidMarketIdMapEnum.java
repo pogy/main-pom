569efa027e94df26eb78e344bfa75ce71845545a
王浩翔 <whx139@qq.com>
2017-10-12 18:11:56 +0800

    排行榜周期扩展

--- main4-monitor/main4-monitor-api/src/main/java/com/shigu/main4/monitor/enums/CidMarketIdMapEnum.java
+++ main4-monitor/main4-monitor-api/src/main/java/com/shigu/main4/monitor/enums/CidMarketIdMapEnum.java
@@ -15,19 +15,23 @@ import java.util.List;
  */
 public enum CidMarketIdMapEnum {
 
-    MAN_CAT_SHOP_RANKING(30L, "man_cat_shop_ranking_score_index_","本周男装排行榜", 608L, 613L, 617L, 619L, 621L, 648L, 650L, 655L, 661L, 670L, 674L, 1087L, 1105L, 1403L),  //男装对应市场
-    WOMAN_CAT_SHOP_RANKING(16L, "woman_cat_shop_ranking_score_index_", "本周女装排行榜",601L, 642L, 652L, 666L, 668L, 1063L, 1462L), //女装对应市场
-    ALL_CAT_SHOP_RANKING(0L, "all_cat_shop_ranking_score_index_","本周档口总排行榜", 601L, 608L, 613L, 617L, 619L, 621L, 642L, 648L, 650L, 652L, 655L, 661L, 666L, 668L, 670L, 674L, 1063L, 1087L, 1105L, 1403L, 1455L, 1462L, 1552L, 1597L); //杭州全部市场
+    MAN_CAT_SHOP_RANKING(30L, "man_cat_shop_ranking_score_index_","本周男装排行榜",RankingPeriodEnum.RANKING_BY_WEEK, 608L, 613L, 617L, 619L, 621L, 648L, 650L, 655L, 661L, 670L, 674L, 1087L, 1105L, 1403L),  //男装对应市场
+    WOMAN_CAT_SHOP_RANKING(16L, "woman_cat_shop_ranking_score_index_", "本周女装排行榜",RankingPeriodEnum.RANKING_BY_WEEK,601L, 642L, 652L, 666L, 668L, 1063L, 1462L), //女装对应市场
+    ALL_CAT_SHOP_RANKING(0L, "all_cat_shop_ranking_score_index_","本周档口总排行榜",RankingPeriodEnum.RANKING_BY_WEEK, 601L, 608L, 613L, 617L, 619L, 621L, 642L, 648L, 650L, 652L, 655L, 661L, 666L, 668L, 670L, 674L, 1063L, 1087L, 1105L, 1403L, 1455L, 1462L, 1552L, 1597L), //杭州全部市场
+    ALL_CAT_SHOP_RANKING_MONTH(0L, "all_cat_shop_ranking_score_month_index_","本月档口总排行榜",RankingPeriodEnum.RANKING_BY_MONTH, 601L, 608L, 613L, 617L, 619L, 621L, 642L, 648L, 650L, 652L, 655L, 661L, 666L, 668L, 670L, 674L, 1063L, 1087L, 1105L, 1403L, 1455L, 1462L, 1552L, 1597L), //杭州全部市场月排行
+    ;
     public final Long cid;
     public final String indexPrefix;
     public final String title;
+    public final RankingPeriodEnum periodEnum;
     private final List<Long> marketIds;
 
-    CidMarketIdMapEnum(Long cid, String indexPrefix,String title, Long... marketIds) {
+    CidMarketIdMapEnum(Long cid, String indexPrefix,String title,RankingPeriodEnum periodEnum, Long... marketIds) {
         this.cid = cid;
         this.indexPrefix = indexPrefix;
         this.marketIds = new ArrayList<Long>();
         this.title = title;
+        this.periodEnum = periodEnum;
         for (Long marketId : marketIds) {
             this.marketIds.add(marketId);
         }
--- main4-monitor/main4-monitor-default/src/test/java/com/shigu/main4/monitor/test/ShopDataInit.java
+++ main4-monitor/main4-monitor-default/src/test/java/com/shigu/main4/monitor/test/ShopDataInit.java
@@ -10,6 +10,7 @@ import com.opentae.data.mall.interfaces.ShiguShopLicenseMapper;
 import com.opentae.data.mall.interfaces.ShiguShopMapper;
 import com.searchtool.configs.ElasticConfiguration;
 import com.shigu.main4.monitor.enums.CidMarketIdMapEnum;
+import com.shigu.main4.monitor.enums.RankingPeriodEnum;
 import com.shigu.main4.monitor.vo.RankingShopVO;
 import com.shigu.main4.tools.RedisIO;
 import org.elasticsearch.action.search.SearchRequestBuilder;
@@ -17,8 +18,6 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.RangeQueryBuilder;
-import org.elasticsearch.search.aggregations.Aggregation;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.terms.LongTerms;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
@@ -29,10 +28,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-import static com.shigu.main4.monitor.service.impl.RankingSimpleServiceImpl.getWeekTimeStamp;
+import static com.shigu.main4.monitor.service.impl.RankingSimpleServiceImpl.getPeriodTimeStamp;
 
 /**
  * 类名：ShopDataInit
@@ -59,17 +61,20 @@ public class ShopDataInit {
 
     @Test
     public void initMan() {
-        upDataToRedis(CidMarketIdMapEnum.ALL_CAT_SHOP_RANKING, 1);
-        upDataToRedis(CidMarketIdMapEnum.MAN_CAT_SHOP_RANKING, 1);
-        upDataToRedis(CidMarketIdMapEnum.WOMAN_CAT_SHOP_RANKING, 1);
-        upDataToRedis(CidMarketIdMapEnum.ALL_CAT_SHOP_RANKING, 0);
-        upDataToRedis(CidMarketIdMapEnum.MAN_CAT_SHOP_RANKING, 0);
-        upDataToRedis(CidMarketIdMapEnum.WOMAN_CAT_SHOP_RANKING, 0);
+        //upDataToRedis(CidMarketIdMapEnum.ALL_CAT_SHOP_RANKING, 1, RankingPeriodEnum.RANKING_BY_WEEK);
+        //upDataToRedis(CidMarketIdMapEnum.MAN_CAT_SHOP_RANKING, 1, RankingPeriodEnum.RANKING_BY_WEEK);
+        //upDataToRedis(CidMarketIdMapEnum.WOMAN_CAT_SHOP_RANKING, 1, RankingPeriodEnum.RANKING_BY_WEEK);
+        //upDataToRedis(CidMarketIdMapEnum.ALL_CAT_SHOP_RANKING, 0, RankingPeriodEnum.RANKING_BY_WEEK);
+        //upDataToRedis(CidMarketIdMapEnum.MAN_CAT_SHOP_RANKING, 0, RankingPeriodEnum.RANKING_BY_WEEK);
+        //upDataToRedis(CidMarketIdMapEnum.WOMAN_CAT_SHOP_RANKING, 0, RankingPeriodEnum.RANKING_BY_WEEK);
+
+        upDataToRedis(CidMarketIdMapEnum.ALL_CAT_SHOP_RANKING_MONTH, 1, RankingPeriodEnum.RANKING_BY_MONTH);
+        upDataToRedis(CidMarketIdMapEnum.ALL_CAT_SHOP_RANKING_MONTH, 0, RankingPeriodEnum.RANKING_BY_MONTH);
     }
 
-    public void upDataToRedis(CidMarketIdMapEnum cat, int pem) {
-        String fromTime = getWeekTimeStamp(pem + 1) + " 00:00:00";
-        String toTime = getWeekTimeStamp(pem) + " 00:00:00";
+    public void upDataToRedis(CidMarketIdMapEnum cat, int pem, RankingPeriodEnum periodEnum) {
+        String fromTime = getPeriodTimeStamp(pem + 1, periodEnum) + " 00:00:00";
+        String toTime = getPeriodTimeStamp(pem, periodEnum) + " 00:00:00";
         Client searchClient = ElasticConfiguration.searchClient;
         SearchRequestBuilder searchRequestBuilder = searchClient.prepareSearch("shigugoodsup").setTypes("hz");
         BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
@@ -116,7 +121,7 @@ public class ShopDataInit {
         //截取算分前200的档口
         rankingShopVOS.sort(new RankingShopVO.RankingShopComparator());
         rankingShopVOS = rankingShopVOS.subList(0, 200 < rankingShopVOS.size() ? 200 : rankingShopVOS.size());
-        List<RankingShopVO> pastRankingShop = redisIO.getList(cat.getIndexPrefix() + getWeekTimeStamp(pem + 1), RankingShopVO.class);
+        List<RankingShopVO> pastRankingShop = redisIO.getList(cat.getIndexPrefix() + getPeriodTimeStamp(pem + 1, periodEnum), RankingShopVO.class);
         int i = 0;
         for (RankingShopVO rankingShopVO : rankingShopVOS) {
             Long shopId = rankingShopVO.getShopId();
@@ -149,7 +154,7 @@ public class ShopDataInit {
             }
         }
         //排行结果保存15天
-        redisIO.putTemp(cat.getIndexPrefix() + getWeekTimeStamp(pem), rankingShopVOS, 3600 * 24 * 15);
+        redisIO.putTemp(cat.getIndexPrefix() + getPeriodTimeStamp(pem, periodEnum), rankingShopVOS, 3600 * 24 * 15);
     }
 
     /**
