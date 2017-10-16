39d4cf6e1504f3d8c613954f0a33b551be029da3
gubest <gubest_1@126.com>
2017-10-16 14:56:19 +0800

    加了菜单处理

---
+++ daifa-server/src/main/java/com/shigu/admin/bo/OrderWaitSendBO.java
@@ -0,0 +1,59 @@
+package com.shigu.admin.bo;
+
+import java.io.Serializable;
+
+/**
+ * @类编号
+ * @类名称：OrderWaitSendBO
+ * @文件路径：com.shigu.admin.bo.OrderWaitSendBO
+ * @内容摘要：
+ * @编码作者：gzy
+ * @创建日期：2017/10/16 12:37
+ * @version: main-pom
+ * @since: main-pom
+ * @commonents:
+ */
+public class OrderWaitSendBO implements Serializable {
+    private Long dfTradeId;//主订单号
+    private String sendName;//发货人
+    /**
+     * 当前页
+     */
+    private String page;
+    /**
+     * 总数量
+     */
+    private Integer count;
+
+    public Long getDfTradeId () {
+        return dfTradeId;
+    }
+
+    public void setDfTradeId (Long dfTradeId) {
+        this.dfTradeId = dfTradeId;
+    }
+
+    public String getSendName () {
+        return sendName;
+    }
+
+    public void setSendName (String sendName) {
+        this.sendName = sendName;
+    }
+
+    public String getPage () {
+        return page;
+    }
+
+    public void setPage (String page) {
+        this.page = page;
+    }
+
+    public Integer getCount () {
+        return count;
+    }
+
+    public void setCount (Integer count) {
+        this.count = count;
+    }
+}
