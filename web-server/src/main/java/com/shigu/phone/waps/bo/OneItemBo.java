c00277184dd6e3221527a1e9fdcde8220f63fbc1
ChenDaHu <cdh@571xz.com>
2017-10-18 15:09:16 +0800

    wap action 更改

---
+++ web-server/src/main/java/com/shigu/phone/waps/bo/OneItemBo.java
@@ -0,0 +1,25 @@
+package com.shigu.phone.waps.bo;
+
+/**
+ * Created by Admin on 2017/10/18.
+ */
+public class OneItemBo {
+    private String webSite;
+    private Long itemId;
+
+    public String getWebSite() {
+        return webSite;
+    }
+
+    public void setWebSite(String webSite) {
+        this.webSite = webSite;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+}
