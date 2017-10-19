00110cb91efb40edbb8fc83498b6c326c777eaf6
zhaochuanyin <81682024@qq.com>
2017-10-19 15:00:37 +0800

    去掉了一些坑

--- web-server/src/main/java/com/shigu/phone/apps/services/PhoneUserService.java
+++ web-server/src/main/java/com/shigu/phone/apps/services/PhoneUserService.java
@@ -30,7 +30,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
-import sun.reflect.misc.FieldUtil;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
