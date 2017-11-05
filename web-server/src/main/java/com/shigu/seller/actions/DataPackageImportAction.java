0de15ee449a4e66ad3c6d6dd9b9088c2c6f5dc9c
zhaochuanyin <81682024@qq.com>
2017-11-05 16:23:01 +0800

    数据包上传

--- web-server/src/main/java/com/shigu/seller/actions/DataPackageImportAction.java
+++ web-server/src/main/java/com/shigu/seller/actions/DataPackageImportAction.java
@@ -1,9 +1,12 @@
 package com.shigu.seller.actions;
 
+import com.shigu.main4.common.exceptions.JsonErrException;
 import com.shigu.main4.common.exceptions.Main4Exception;
+import com.shigu.main4.item.exceptions.ItemModifyException;
 import com.shigu.main4.tools.RedisIO;
 import com.shigu.main4.ucenter.services.PackageImportGoodsDataService;
 import com.shigu.main4.ucenter.vo.ShiguGoodsTinyVO;
+import com.shigu.seller.services.DataPackageImportService;
 import com.shigu.seller.vo.PackageVO;
 import com.shigu.session.main4.PersonalSession;
 import com.shigu.session.main4.ShopSession;
@@ -42,8 +45,30 @@ public class DataPackageImportAction {
     @Autowired
     PackageImportGoodsDataService packageImportGoodsDataService;
 
+    @Autowired
+    DataPackageImportService dataPackageImportService;
+
+    final String KEY_FLAG="DATAPACKAGE_";
+    final int KEY_TIME=3600*2;
 
 
+    @RequestMapping("seller/doUploadGoodsData")
+    @ResponseBody
+    public JSONObject doUploadGoodsData(Long goodsId,HttpSession session) throws JsonErrException {
+        PersonalSession ps = (PersonalSession) session.getAttribute(SessionEnum.LOGIN_SESSION_USER.getValue());
+        ShopSession logshop = ps.getLogshop();
+        //得到商品
+        ShiguGoodsTinyVO tiny=redisIO.get(KEY_FLAG+goodsId,ShiguGoodsTinyVO.class);
+        if (tiny == null) {
+            throw new JsonErrException("ID不存在");
+        }
+        try {
+            dataPackageImportService.addToXzw(logshop.getShopId(),tiny);
+        } catch (ItemModifyException e) {
+            throw new JsonErrException(e.getMessage());
+        }
+        return JsonResponseUtil.success();
+    }
 
     /**
      * ====================================================================================
@@ -65,10 +90,10 @@ public class DataPackageImportAction {
 
         List<ShiguGoodsTinyVO> list= packageImportGoodsDataService.packageImporttempGoods (goodsPackageUrl, logshop.getShopId (), "rest");
         String se=UUID.randomUUID ().toString ();
-        redisIO.putTemp ("packageList"+logshop.getShopId ()+se,list,1000*60*2);
+        redisIO.putTemp ("packageList"+logshop.getShopId ()+se,list,KEY_TIME);
         if(list.size ()>0) {
             for (ShiguGoodsTinyVO goodsVO : list) {
-                redisIO.putTemp(goodsVO.getGoodsId ()+"",goodsVO,1000*60*60*2);
+                redisIO.putTemp(KEY_FLAG+goodsVO.getGoodsId (),goodsVO,KEY_TIME);
             }
 
         }
