451ba0385aad16b25382176d246b31c6aa9fbff9
ChenDaHu <cdh@571xz.com>
2017-11-21 14:16:01 +0800

    任性送现金

--- web-server/src/main/java/com/shigu/activity/actions/ActivityAction.java
+++ web-server/src/main/java/com/shigu/activity/actions/ActivityAction.java
@@ -60,7 +60,13 @@ public class ActivityAction {
 
     @Autowired
     private DrawQualification newAutumnDrawQualification;
-
+    /**
+     * 任性送现金活动
+     */
+    @RequestMapping("activity/cash")
+    public String cash(){
+        return "activity/cash";
+    }
     /**
      * 发现好货
      *
