dded5317fbef5610bff2c05694da0c70235a1472
powell <124088388@qq.com>
2017-12-13 21:42:33 +0800

    修改算法

--- web-server/src/main/java/com/shigu/buyer/actions/MemberAction.java
+++ web-server/src/main/java/com/shigu/buyer/actions/MemberAction.java
@@ -886,7 +886,7 @@ public class MemberAction {
             throw new JsonErrException("请输入正确的金额");
         }
         //单位 元->分，然后计算出手续费 目前为6%，不足1分部分由用户补齐
-        return JsonResponseUtil.success().element("userRealWithdrawMoney", String.format("%.2f",(userWirteMoney * 100 * 994 / 1000) * 0.01));
+        return JsonResponseUtil.success().element("userRealWithdrawMoney", String.format("%.2f",(userWirteMoney  * 994 / 10) * 0.01));
     }
 
     /**
