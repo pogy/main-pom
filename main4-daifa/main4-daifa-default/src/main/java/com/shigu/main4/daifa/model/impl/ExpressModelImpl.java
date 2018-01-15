2be1bea3b4d1b691893f2ccc20653a4032c05344
gtx <601724080@qq.com>
2018-01-15 18:07:20 +0800

    调用快递鸟异常处理方式修改

--- main4-daifa/main4-daifa-default/src/main/java/com/shigu/main4/daifa/model/impl/ExpressModelImpl.java
+++ main4-daifa/main4-daifa-default/src/main/java/com/shigu/main4/daifa/model/impl/ExpressModelImpl.java
@@ -18,6 +18,7 @@ import com.shigu.main4.daifa.utils.KdHttpUtil;
 import com.shigu.main4.daifa.vo.ExpressVO;
 import com.shigu.main4.daifa.vo.QueryPostCodeVO;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
@@ -266,6 +267,9 @@ public class ExpressModelImpl implements ExpressModel {
             throw new KdApiException(KdApiException.KdApiExceptionEnum.SIGN_ERROR);
         }
         String result = KdHttpUtil.sendPost(kdConfig.getEorderserviceReqURL(), params);
+        if (StringUtils.isBlank(result)) {
+            throw new KdApiException(KdApiException.KdApiExceptionEnum.API_ERROR);
+        }
 //System.out.println("@@@@@@@@@@@@@@"+result);
         JSONObject obj = JSONObject.fromObject(result);
 
