a5a5992360e83a69dc6bb7a41e55e39c1dc13fa2
zf <460333383@qq.com>
2018-01-25 10:37:49 +0800

    删除京东dubbo

--- main4-jd/main4-jd-api/src/main/java/com/shigu/main4/jd/exceptions/JdApiException.java
+++
@@ -1,7 +0,0 @@
-package com.shigu.main4.jd.exceptions;
-
-public class JdApiException extends Exception{
-    public JdApiException(String message) {
-        super(message);
-    }
-}
--- main4-jd/main4-jd-default/src/test/java/CreateTest.java
+++
@@ -1,9 +0,0 @@
-import com.opentae.core.mybatis.utils.DDL2Bean;
-
-import java.io.IOException;
-
-public class CreateTest {
-    public static void main(String[] args) throws IOException {
-        DDL2Bean.main(args);
-    }
-}
