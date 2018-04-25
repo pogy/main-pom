3f1ac30a654bee9bb11e258ae6bd2aa47aadd0b2
zf <460333383@qq.com>
2018-04-25 10:48:54 +0800

    图片上传接口

---
+++ web-server/src/main/java/com/shigu/photo/actions/PhotoImgAction.java
@@ -0,0 +1,27 @@
+package com.shigu.photo.actions;
+
+import com.shigu.photo.process.PhotoImgProcess;
+import com.shigu.session.main4.PersonalSession;
+import com.shigu.session.main4.names.SessionEnum;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
+
+@Controller
+@RequestMapping("photo/")
+public class PhotoImgAction {
+    @Autowired
+    PhotoImgProcess photoImgProcess;
+
+    @RequestMapping("selPhotoToken")
+    @ResponseBody
+    public JSONObject selPhotoToken(HttpSession session) throws UnsupportedEncodingException {
+        PersonalSession ps= (PersonalSession) session.getAttribute(SessionEnum.LOGIN_SESSION_USER.getValue());
+        return JSONObject.fromObject(photoImgProcess.selPhotoToken(ps.getUserId())).element("result", "success");
+    }
+}
