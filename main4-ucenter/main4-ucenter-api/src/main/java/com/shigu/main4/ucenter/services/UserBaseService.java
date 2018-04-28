ae050d2a685661941c056fc5799aea56b50ec0b9
gtx <601724080@qq.com>
2018-04-28 10:43:19 +0800

    删除重复定义的方法

--- main4-ucenter/main4-ucenter-api/src/main/java/com/shigu/main4/ucenter/services/UserBaseService.java
+++ main4-ucenter/main4-ucenter-api/src/main/java/com/shigu/main4/ucenter/services/UserBaseService.java
@@ -27,13 +27,6 @@ public interface UserBaseService {
     PersonalSession selUserForSessionByUserIdAndNick(Long userId,String tbNick);
 
     /**
-     * 按用户ID和tbNick查用户淘宝相关基准信息
-     * @param userId
-     * @return
-     */
-    PersonalSession selUserForSessionByUserIdAndNick(Long userId,String tbNick);
-
-    /**
      * 按用户名查用户基准信息
      * @param userName
      * @return
