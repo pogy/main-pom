b62b25c96aad4d2b84bc008aa1e3b5a67f3af971
zf <460333383@qq.com>
2018-06-06 09:36:48 +0800

    修复多个自定义vid相同时,生成ID可能存在错误的bug

--- main4-ucenter/main4-ucenter-default/src/main/java/com/shigu/main4/ucenter/util/ImportCsvFileService.java
+++ main4-ucenter/main4-ucenter-default/src/main/java/com/shigu/main4/ucenter/util/ImportCsvFileService.java
@@ -155,23 +155,27 @@ public class ImportCsvFileService {
                     List<String> hasProps=Arrays.stream(((String) codeValueMap.get("cateProps")).split(";")).filter(StringUtils::isNotBlank).collect(Collectors.toList());
                     List<String> imgs= Arrays.stream(((String)codeValueMap.get("picture")).split(";")).filter(StringUtils::isNotBlank).collect(Collectors.toList());
 //                    List<String> skus=Arrays.stream(((String)codeValueMap.get("skuProps")).split(";")).filter(StringUtils::isNotBlank).collect(Collectors.toList());
-                    List<String> hasInputVid=new ArrayList<>();
+                    List<Integer> hasInputVid=new ArrayList<>();
                     for (String anInputList : inputList) {
                         String[] inputs = anInputList.split(":");
-                        hasInputVid.add(inputs[1]);
+                        Integer nvid=new Integer(inputs[1]);
+                        if(nvid>0){
+                            nvid=-nvid;
+                        }
+                        hasInputVid.add(nvid);
                     }
                     Integer startIndex=null;
                     if(hasInputVid.size()>0){
-                        hasInputVid.sort((o1, o2) -> new Integer(o2)-new Integer(o1));
-                        startIndex=new Integer(hasInputVid.get(0));
+                        hasInputVid.sort(Comparator.comparingInt(o -> o));
+                        startIndex= hasInputVid.get(0);
                     }
                     if(startIndex==null){
                         startIndex=0;
                     }
                     for(int iv=0;iv<inputList.size();iv++){
                         String[] inputs=inputList.get(iv).split(":");
-                        if(hasInputVid.contains(inputs[1])){
-                            hasInputVid.removeIf(inputs[1]::equals);
+                        if(hasInputVid.contains(new Integer(inputs[1]))){
+                            hasInputVid.removeIf(new Integer(inputs[1])::equals);
                         }else{
                             String newVid=(startIndex-(iv+1))+"";
                             String input=inputs[0]+":"+newVid+":"+inputs[2];
