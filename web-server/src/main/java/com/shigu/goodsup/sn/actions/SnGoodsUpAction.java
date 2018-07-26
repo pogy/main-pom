71a51e17c1d84c4db19c12f0287931d3240035a8
尤祥官 <yxg@571xz.com>
2018-07-26 15:14:14 +0800

    苏宁类目选择更新

--- web-server/src/main/java/com/shigu/goodsup/sn/actions/SnGoodsUpAction.java
+++ web-server/src/main/java/com/shigu/goodsup/sn/actions/SnGoodsUpAction.java
@@ -15,10 +15,7 @@ import com.shigu.goodsup.jd.service.JdUpItemService;
 import com.shigu.goodsup.sn.service.SnCategoryService;
 import com.shigu.goodsup.sn.service.SnUpItemService;
 import com.shigu.goodsup.sn.service.SnUserInfoService;
-import com.shigu.goodsup.sn.vo.PropItemVo;
-import com.shigu.goodsup.sn.vo.PropValueVo;
-import com.shigu.goodsup.sn.vo.SnPageItem;
-import com.shigu.goodsup.sn.vo.SnShowDataVo;
+import com.shigu.goodsup.sn.vo.*;
 import com.shigu.main4.common.exceptions.Main4Exception;
 import com.shigu.main4.ucenter.services.UserBaseService;
 import com.shigu.session.main4.PersonalSession;
@@ -82,7 +79,7 @@ public class SnGoodsUpAction {
      * 上传页面
      */
     @RequestMapping("publish")
-    public String publish(Long itemId, HttpServletRequest request, HttpSession session, Map<String, Object> map) {
+    public String publish(Long itemId, HttpServletRequest request, HttpSession session, Map<String, Object> map, String cid) {
         try {
             PersonalSession ps = (PersonalSession) session.getAttribute(SessionEnum.LOGIN_SESSION_USER.getValue());
             if (ps == null || ps.getLoginFromType() != LoginFromType.SN) {
@@ -90,76 +87,94 @@ public class SnGoodsUpAction {
                 return "redirect:http://www.571xz.com/ortherLogin.htm?ortherLoginType=8&backUrl=" + URLEncoder.encode(request.getRequestURL().toString() +
                         (queryString == null ? "" : ("?" + queryString)), "utf-8");
             }
-            String SnUsername=snUserInfoService.getSnUsernameBySubUid(ps.getSubUserId());
+            String SnUsername = snUserInfoService.getSnUsernameBySubUid(ps.getSubUserId());
             Subject currentUser = SecurityUtils.getSubject();
             if (currentUser.hasRole(RoleEnum.STORE.getValue())) {
                 throw new CustomException("档口不支持代理功能");
             }
             int total = Integer.valueOf(snCategoryService.getCategory(SnUsername).getTotalSize());
-            if(total==0){
+            if (total == 0) {
                 throw new CustomException("不是苏宁商家");
             }
-            SnCategoryResponse response=snCategoryService.getCategory(SnUsername, itemId);
+            SnCategoryResponse response = snCategoryService.getCategory(SnUsername, itemId);
             List<CategoryQueryResponse.CategoryQuery> categoryQueries = response.getCategoryQueryList();
-            if(categoryQueries.size()!=1){
-                return "suning/catChoose";
-            }else {
-                if(response.getCname()!=null) {
-                    if (!categoryQueries.get(0).getDescPath().contains(response.getCname())){
-                        map.put("errmsg","商品类目与店内类目不匹配");
-                        return "suning/uperror";
+            String categoryCode;
+            if (cid == null) {
+                if (categoryQueries.size() != 1) {
+                    List<SnCatVo> list = new ArrayList<>();
+                    for (CategoryQueryResponse.CategoryQuery c : categoryQueries) {
+                        SnCatVo vo = new SnCatVo();
+                        vo.setCid(c.getCategoryCode());
+                        vo.setCname(c.getDescPath());
+                        list.add(vo);
                     }
+                    SnQueryVo snQueryVo = new SnQueryVo(categoryQueries.get(0).getCategoryCode(), itemId);
+                    map.put("cates", list);
+                    map.put("query", snQueryVo);
+                    return "suning/changeGoodsCate";
                 }
-                String categoryCode=categoryQueries.get(0).getCategoryCode();
-                List<NewbrandQueryResponse.QueryNewbrand> brands= snUserInfoService.getBrand(SnUsername,categoryCode);
-                if(brands==null){
-                    throw new CustomException("没有苏宁品牌,暂不支持上传");
-                }
-                SnPageItem snPageItem=snUpItemService.findGoods(itemId);
-                if(snPageItem==null){
-                    map.put("errmsg","商品不存在");
+                categoryCode = categoryQueries.get(0).getCategoryCode();
+            } else {
+                categoryCode = cid;
+            }
+            if (response.getCname() != null) {
+                if (!categoryQueries.get(0).getDescPath().contains(response.getCname())) {
+                    map.put("errmsg", "商品类目与店内类目不匹配");
                     return "suning/uperror";
                 }
-                if(snPageItem.getItem().getTitle()!=null){
-                    snPageItem.setTitleLength(snPageItem.getItem().getTitle().getBytes(Charset.forName("GBK")).length);
-                }
-                if(snPageItem.getItem().getSellPoint()!=null){
-                    snPageItem.setSellPointLength(snPageItem.getItem().getSellPoint().getBytes(Charset.forName("GBK")).length);
-                }
-                SnShowDataVo snShowDataVo=new SnShowDataVo();
-                snShowDataVo.setUsername(SnUsername);
-                snShowDataVo.setSnPageItem(snPageItem);
-                snShowDataVo.setDeliveyList(snUpItemService.selPostModel(SnUsername));
-                snShowDataVo.setStoreCats(snUpItemService.selShopCats(SnUsername));
-                snShowDataVo.setGoodsCat(categoryQueries.get(0).getDescPath().replace("|",">"));
-                snShowDataVo.setPropsVo(snUpItemService.selProps(itemId,categoryCode,SnUsername,brands));
-                int size=0;
-                for(PropItemVo p:snShowDataVo.getPropsVo().getSaleProps()){
-                    for(PropValueVo propValueVo:p.getValues()){
+            }
+            List<NewbrandQueryResponse.QueryNewbrand> brands = snUserInfoService.getBrand(SnUsername, categoryCode);
+            if (brands == null) {
+                throw new CustomException("没有苏宁品牌,暂不支持上传");
+            }
+            SnPageItem snPageItem = snUpItemService.findGoods(itemId);
+            if (snPageItem == null) {
+                map.put("errmsg", "商品不存在");
+                return "suning/uperror";
+            }
+            if (snPageItem.getItem().getTitle() != null) {
+                snPageItem.setTitleLength(snPageItem.getItem().getTitle().getBytes(Charset.forName("GBK")).length);
+            }
+            if (snPageItem.getItem().getSellPoint() != null) {
+                snPageItem.setSellPointLength(snPageItem.getItem().getSellPoint().getBytes(Charset.forName("GBK")).length);
+            }
+            SnShowDataVo snShowDataVo = new SnShowDataVo();
+            snShowDataVo.setUsername(SnUsername);
+            snShowDataVo.setSnPageItem(snPageItem);
+            snShowDataVo.setDeliveyList(snUpItemService.selPostModel(SnUsername));
+            snShowDataVo.setStoreCats(snUpItemService.selShopCats(SnUsername));
+            snShowDataVo.setGoodsCat(categoryQueries.get(0).getDescPath().replace("|", ">"));
+            snShowDataVo.setPropsVo(snUpItemService.selProps(itemId, categoryCode, SnUsername, brands));
+            int size = 0;
+            for (PropItemVo p : snShowDataVo.getPropsVo().getSaleProps()) {
+                for (PropValueVo propValueVo : p.getValues()) {
 
-                        if(propValueVo.isSelected()){
-                            size++;
-                        }
+                    if (propValueVo.isSelected()) {
+                        size++;
                     }
                 }
-
-                SnToken snToken=new SnToken();
-                snToken.setUsername(SnUsername);
-                snToken.setCreateTime(new Date());
-                String tokenStr=Opt3Des.encryptPlainData(JSONObject.fromObject(snToken).toString());
-                snShowDataVo.setToken(tokenStr);
-                map.put("allData",snShowDataVo);
-                map.put("selectedSizeLength",size);
-                map.put("id",itemId);
-                map.put("categoryCode",categoryCode);
-                map.put("sn_yj_sn_session",ps);
-                return "suning/sn";
             }
-        } catch (Exception e) {
+
+            SnToken snToken = new SnToken();
+            snToken.setUsername(SnUsername);
+            snToken.setCreateTime(new Date());
+            String tokenStr = Opt3Des.encryptPlainData(JSONObject.fromObject(snToken).toString());
+            snShowDataVo.setToken(tokenStr);
+            map.put("allData", snShowDataVo);
+            map.put("selectedSizeLength", size);
+            map.put("id", itemId);
+            map.put("categoryCode", categoryCode);
+            map.put("sn_yj_sn_session", ps);
+            return "suning/sn";
+        } catch (
+                Exception e)
+
+        {
             e.printStackTrace();
             map.put("errmsg", "该商品暂不支持上传");
             return "suning/uperror";
         }
+
     }
 
     /**
@@ -178,19 +193,19 @@ public class SnGoodsUpAction {
     }
 
     @RequestMapping("callback")
-    public String SnCallback(String code, HttpServletRequest request, HttpSession session) throws Main4Exception{
+    public String SnCallback(String code, HttpServletRequest request, HttpSession session) throws Main4Exception {
 //        System.out.println(code);
         JSONObject jsonObject = JSONObject.fromObject(Opt3Des.decryptPlainData(code.replace(" ", "+")));
         SnAuthInfoResponse snAuthInfoResponse = (SnAuthInfoResponse) JSONObject.toBean(jsonObject, SnAuthInfoResponse.class);
         SnTokenInfo snTokenInfo = snAuthInfoResponse.getData();
         Subject currentUser = SecurityUtils.getSubject();
-        String sun= snTokenInfo.getSuningUserName();
+        String sun = snTokenInfo.getSuningUserName();
         CaptchaUsernamePasswordToken captchaUsernamePasswordToken = new CaptchaUsernamePasswordToken(sun, null, true, request.getRemoteAddr(), "", UserType.MEMBER);
         captchaUsernamePasswordToken.setLoginFromType(LoginFromType.SN);
         captchaUsernamePasswordToken.setSubKey(sun);
-        try{
+        try {
             currentUser.login(captchaUsernamePasswordToken);
-            PersonalSession personalSession = userBaseService.selUserForSessionByUserName(sun,sun, LoginFromType.SN);
+            PersonalSession personalSession = userBaseService.selUserForSessionByUserName(sun, sun, LoginFromType.SN);
             if (personalSession == null || personalSession.getUserId() == null) {
                 //还是检查一遍避免 字符串+null 出现
                 throw new Main4Exception("授权失败");
@@ -198,10 +213,10 @@ public class SnGoodsUpAction {
             String backUrl = (String) session.getAttribute(SessionEnum.OTHEER_LOGIN_CALLBACK.getValue());
             session.removeAttribute(SessionEnum.OTHEER_LOGIN_CALLBACK.getValue());
             return "redirect:" + loginSuccessUrl(backUrl);
-        }catch (LoginAuthException e){
-            if(e.getMsgback().equals(LoginErrorEnum.TO_BIND_XZUSER)){//还没绑定星座网用户,去绑定一下
+        } catch (LoginAuthException e) {
+            if (e.getMsgback().equals(LoginErrorEnum.TO_BIND_XZUSER)) {//还没绑定星座网用户,去绑定一下
                 return "redirect:/userPhoneBind.htm";
-            }else{
+            } else {
                 throw e;
             }
         }
@@ -215,11 +230,11 @@ public class SnGoodsUpAction {
      */
     private String loginSuccessUrl(String backUrl) {
         String toUrl;
-        if(backUrl != null && backUrl.equals("")){
-            toUrl="http://www.571xz.com";
-        }else if (!"http://www.571xz.com/".equals(backUrl)) {//TODO 首页的,等页面改好,这里再去掉
+        if (backUrl != null && backUrl.equals("")) {
+            toUrl = "http://www.571xz.com";
+        } else if (!"http://www.571xz.com/".equals(backUrl)) {//TODO 首页的,等页面改好,这里再去掉
             toUrl = backUrl;
-        }else {
+        } else {
             //如果个人,跳到这个,如果商户跳到seller
             toUrl = memberFilter.getSuccessUrl();
         }
