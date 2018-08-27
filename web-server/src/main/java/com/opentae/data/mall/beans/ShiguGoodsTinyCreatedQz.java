cc7644ca2271c91017a36e0c7bc9dd1578468b93
尤祥官 <yxg@571xz.com>
2018-08-27 20:01:39 +0800

    泉州站设置今日新品

---
+++ web-server/src/main/java/com/opentae/data/mall/beans/ShiguGoodsTinyCreatedQz.java
@@ -0,0 +1,51 @@
+package com.opentae.data.mall.beans;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+import java.io.Serializable;
+
+public class ShiguGoodsTinyCreatedQz implements Serializable {
+    /**
+     * 
+     */
+    @Id
+    @GeneratedValue(generator = "JDBC")
+	private Long goodsId;
+
+    /**
+     * 
+     */
+	private Date oldCreated;
+
+    /**
+     * 
+     */
+	private Date gmtCreate;
+
+
+	public void setGoodsId(Long goodsId) {
+		this.goodsId = goodsId;
+	}
+
+	public Long getGoodsId() {
+		return this.goodsId;
+	}
+
+	public void setOldCreated(Date oldCreated) {
+		this.oldCreated = oldCreated;
+	}
+
+	public Date getOldCreated() {
+		return this.oldCreated;
+	}
+
+	public void setGmtCreate(Date gmtCreate) {
+		this.gmtCreate = gmtCreate;
+	}
+
+	public Date getGmtCreate() {
+		return this.gmtCreate;
+	}
+
+}
