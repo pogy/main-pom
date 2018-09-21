015b1cb441db111f57cfa190c817514a01d1aee3
zf <460333383@qq.com>
2018-09-21 13:23:08 +0800

    修复活动页不显示市场档口的问题

--- web-server/src/main/java/com/shigu/activity/service/MarketActivityService.java
+++ web-server/src/main/java/com/shigu/activity/service/MarketActivityService.java
@@ -1,14 +1,12 @@
 package com.shigu.activity.service;
 
-import com.opentae.data.mall.beans.ShiguGoodsTiny;
-import com.opentae.data.mall.beans.ShiguMarketActivityGoods;
-import com.opentae.data.mall.beans.ShiguMarketActivitys;
+import com.opentae.core.mybatis.utils.FieldUtil;
+import com.opentae.data.mall.beans.*;
 import com.opentae.data.mall.examples.ShiguGoodsTinyExample;
 import com.opentae.data.mall.examples.ShiguMarketActivityGoodsExample;
-import com.opentae.data.mall.interfaces.ShiguGoodsTinyMapper;
-import com.opentae.data.mall.interfaces.ShiguMarketActivityGoodsMapper;
-import com.opentae.data.mall.interfaces.ShiguMarketActivitysMapper;
-import com.opentae.data.mall.interfaces.ShiguShopLicenseMapper;
+import com.opentae.data.mall.examples.ShiguMarketExample;
+import com.opentae.data.mall.examples.ShiguShopExample;
+import com.opentae.data.mall.interfaces.*;
 import com.shigu.activity.vo.ActivityGoodsVo;
 import com.shigu.main4.common.tools.ShiguPager;
 import com.shigu.main4.common.util.BeanMapper;
@@ -17,7 +15,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Vector;
+import java.util.stream.Collectors;
 
 /**
  * @ProjectName: main-pom
@@ -35,7 +35,10 @@ public class MarketActivityService {
     private ShiguMarketActivityGoodsMapper shiguMarketActivityGoodsMapper;
     @Autowired
     private ShiguGoodsTinyMapper shiguGoodsTinyMapper;
-
+    @Autowired
+    private ShiguMarketMapper shiguMarketMapper;
+    @Autowired
+    private ShiguShopMapper shiguShopMapper;
     public ShiguPager<ActivityGoodsVo> selActivityGoods(Long maId ,Integer pageNo, Integer pageSize){
         ShiguPager<ActivityGoodsVo> pager = new ShiguPager<>();
         if (pageNo==null || pageNo < 1) {
@@ -66,18 +69,35 @@ public class MarketActivityService {
         ShiguGoodsTinyExample tinyExample = new ShiguGoodsTinyExample();
         tinyExample.setWebSite("hz");
         tinyExample.createCriteria().andGoodsIdIn(goodsIds);
-        List<ShiguGoodsTiny> shiguGoodsTinyList = shiguGoodsTinyMapper.selectFieldsByExample(tinyExample, "goods_id,pic_url,parent_market_name,store_num,store_id,pi_price_string,title");
+        List<ShiguGoodsTiny> shiguGoodsTinyList = shiguGoodsTinyMapper.selectFieldsByExample(tinyExample, FieldUtil.codeFields("goods_id,pic_url,parent_market_id,store_id,pi_price_string,title"));
         List<ActivityGoodsVo> voList = new ArrayList<>();
-        for (int i = 0; i <shiguGoodsTinyList.size() ; i++) {
+        List<Long> marketIds=new ArrayList<>();
+        List<Long> shopIds=new ArrayList<>();
+        for (ShiguGoodsTiny tiny : shiguGoodsTinyList) {
             ActivityGoodsVo vo = new ActivityGoodsVo();
-            vo.setGoodsId(shiguGoodsTinyList.get(i).getGoodsId());
-            vo.setImgSrc(shiguGoodsTinyList.get(i).getPicUrl());
-            vo.setMarketName(shiguGoodsTinyList.get(i).getParentMarketName());
-            vo.setPiPriceString(shiguGoodsTinyList.get(i).getPiPriceString());
-            vo.setShopId(shiguGoodsTinyList.get(i).getStoreId());
-            vo.setShopNum(shiguGoodsTinyList.get(i).getStoreNum());
-            vo.setTitle(shiguGoodsTinyList.get(i).getTitle());
+            vo.setGoodsId(tiny.getGoodsId());
+            vo.setImgSrc(tiny.getPicUrl());
+            vo.setPiPriceString(tiny.getPiPriceString());
+            vo.setShopId(tiny.getStoreId());
+            vo.setMarketId(tiny.getParentMarketId());
+            vo.setTitle(tiny.getTitle());
+            marketIds.add(tiny.getParentMarketId());
+            shopIds.add(tiny.getStoreId());
             voList.add(vo);
+        }
+        if(marketIds.size()>0){
+            ShiguMarketExample shiguMarketExample=new ShiguMarketExample();
+            shiguMarketExample.createCriteria().andMarketIdIn(marketIds);
+            List<ShiguMarket> ms=shiguMarketMapper.selectFieldsByExample(shiguMarketExample, FieldUtil.codeFields("market_id,market_name"));
+            Map<Long,String> mmap=ms.stream().collect(Collectors.toMap(ShiguMarket::getMarketId,ShiguMarket::getMarketName));
+            voList.forEach(activityGoodsVo -> activityGoodsVo.setMarketName(mmap.get(activityGoodsVo.getMarketId())));
+        }
+        if(shopIds.size()>0){
+            ShiguShopExample shiguShopExample=new ShiguShopExample();
+            shiguShopExample.createCriteria().andShopIdIn(shopIds);
+            List<ShiguShop> shops=shiguShopMapper.selectFieldsByExample(shiguShopExample,FieldUtil.codeFields("shop_id,shop_num"));
+            Map<Long,String> smap=shops.stream().collect(Collectors.toMap(ShiguShop::getShopId,ShiguShop::getShopNum));
+            voList.forEach(activityGoodsVo -> activityGoodsVo.setShopNum(smap.get(activityGoodsVo.getShopId())));
         }
         pager.setContent(voList);
         return pager;
