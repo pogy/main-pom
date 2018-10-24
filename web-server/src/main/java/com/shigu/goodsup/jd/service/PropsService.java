a10f22889bd9bc389bba498f93062d794a1e4658
zf <460333383@qq.com>
2018-10-24 15:27:57 +0800

    还原京东代码

--- web-server/src/main/java/com/shigu/goodsup/jd/service/PropsService.java
+++ web-server/src/main/java/com/shigu/goodsup/jd/service/PropsService.java
@@ -49,6 +49,224 @@ public class PropsService {
     NewShowForCdnService newShowForCdnService;
 
     /**
+     * 计算页面要显示的属性列
+     * @param cid 类目ID
+     * @return
+     */
+
+    public PropsVO selProps(Long cid){
+        Cache cache=ehCacheManager.getCache("jdProps");
+        PropsVO propsVO=cache.get("prop_"+cid,PropsVO.class);
+        if(propsVO!=null){
+            return propsVO;
+        }
+        propsVO=new PropsVO();
+        propsVO.setCid(cid);
+        //开始处理品牌
+        List<ItemProp> itemPropresponse= selItemProp(cid);//去接口里面调,有可能等于null
+        for(ItemProp tip:itemPropresponse){
+            if(tip.getPid()==20000L||"货号".equals(tip.getName())||tip.getPid()==13021751L){
+                //如果是品牌货号
+                continue;
+            }
+
+            PropertyItemVO piv=new PropertyItemVO();
+            piv.setPid(tip.getPid());
+            piv.setName(tip.getName());
+            piv.setMustHave(tip.getMust());
+            piv.setCanAlias(tip.getIsAllowAlias());
+            piv.setType(selType(tip.getIsEnumProp(),tip.getIsItemProp(),tip.getIsMaterial(),tip.getMulti(),tip.getIsInputProp()));
+            //**********************加值**************************
+            if(!piv.getType().equals(PropType.INPUT)){//不是输入框的情况下
+                piv.addPropValueList(parseTaobaoPropValueToVO(tip.getPropValues()));
+            }
+            //放入propsVO
+            if(tip.getIsColorProp()){
+                //如果是颜色
+                propsVO.setColor(piv);
+            }else if(tip.getIsSaleProp()){
+                //如果是销售属性
+                propsVO.addSaleProps(piv);
+            }else{
+                //一般属性
+                propsVO.addProperties(piv);
+            }
+        }
+        cache.put("prop_"+cid,propsVO);
+        return propsVO;
+    }
+
+
+
+    /**
+     * 导入item里面的value
+     * @param propsVO 类目属性
+     * @param propName 类目属性串
+     * @param propImgs 属性图
+     * @param alias 类目属性别名
+     * @param inputStr 自定义字符串
+     * @param inputIds 自定义ID串
+     * @return
+     */
+    public PropsVO importValue(PropsVO propsVO, String propName, List<PropImg> propImgs, String alias, String inputStr, String inputIds,Long itemId) throws IOException, ClassNotFoundException, CloneNotSupportedException {
+        //**************先把propsVO克隆一把,以免破坏缓存
+        PropsVO psv=propsVO.deepClone();
+        //把所有属性key\value化
+        Map<Long,PropertyItemVO> map=new HashMap<>();//key为pid
+        //把颜色放入
+        if(psv.getColor()!=null){
+            map.put(psv.getColor().getPid(),psv.getColor());
+        }
+        //把一般属性放入
+        if(psv.getProperties()!=null){
+            for(PropertyItemVO piv:psv.getProperties()){
+                map.put(piv.getPid(),piv);
+            }
+        }
+        //把销售属性放入
+        if(psv.getSaleProps()!=null){
+            for(PropertyItemVO piv:psv.getSaleProps()){
+                map.put(piv.getPid(),piv);
+            }
+        }
+        //**************解析***************************
+        //把propImgs解析成key-value
+        Map<String,String> propUrlMap=new HashMap<>();
+        if(propImgs!=null){
+            for(PropImg p:propImgs){
+                propUrlMap.put(p.getProperties(),p.getUrl());
+            }
+        }
+        //在初级没有匹配到的,可能二级需要。key为pid,value为vid
+        TempErProp nonmap=new TempErProp();
+        //input可能的选项,因为标准values里不存在
+        TempOtProp nonpv=new TempOtProp();
+        //先解析props把选项搞上
+        if(propName!=null&&!"".equals(propName)){
+            String[] proparr=propName.split(";");
+            for(String p:proparr){
+                String[] parr=p.split(":");
+                if(parr.length<2){//不是标准pid:vid格式
+                    continue;
+                }
+                //取得这个属性
+                PropertyItemVO piv=map.get(Long.valueOf(parr[0]));
+                if(piv==null){//如果属性不存在,有可能是二级属性
+                    nonmap.addSelByPidandVid(Long.valueOf(parr[0]),Long.valueOf(parr[1]));
+                    continue;
+                }
+                PropertyValueVO pvv=new PropertyValueVO();
+                pvv.setSelected(true);
+                pvv.setVid(Long.valueOf(parr[1]));
+                pvv.setImgUrl(propUrlMap.get(piv.getPid()+":"+pvv.getVid()));
+                pvv.setName(parr[3]);
+                int result=piv.addPropValue(pvv);
+                if(result==1){
+                    nonpv.addByPidandVid(piv.getPid(),pvv.getVid());
+                }
+            }
+        }
+        //处理alias
+        if(alias!=null&&!"".equals(alias)){
+            String[] aliasarr=alias.split(";");
+            for(String ai:aliasarr){
+                String[] aiarr=ai.split(":");
+                if(aiarr.length<3){//不是标准的pid:vid:name格式,需要舍去
+                    continue;
+                }
+                //如果是二级的属性
+                Long pid=Long.valueOf(aiarr[0]);//pid
+                Long vid=Long.valueOf(aiarr[1]);//vid
+                //取得属性
+                PropertyItemVO piv=map.get(pid);
+                if(piv==null){//说明不在props总串里,略去
+                    log.error("cid="+psv.getCid()+"  pid="+pid+" in alias but not in props");
+                    continue;
+                }
+                //从标准类目选项中取
+                PropertyValueVO pvv=piv.getPropValueByVid(vid);
+                if(pvv==null){//标准类目选项中如果没有,那么不应该是alias,应该是input,所以这里如果真没有,忽略
+                    log.error("cid="+psv.getCid()+"  pid="+piv.getPid()+" in alias but have no propvalue");
+                    continue;
+                }
+                pvv.setName(aiarr[2]);
+                //把pvv的变化,加到属性中去,这个add,如果有了是更新
+                piv.addPropValue(pvv);
+            }
+        }
+
+        //处理input
+        if(inputIds!=null&&!"".equals(inputIds)
+                &&inputStr!=null){
+            String[] idarr=inputIds.split(",");
+            String[] strarr=inputStr.split(",");
+            if(idarr.length==strarr.length){//长度不一至的,日志一下,一般这种就是人为修改,改出来的。淘宝来的数据不可能这样
+                //按ID,一个个进行设置
+                for(int i=0;i<idarr.length;i++){
+                    Long pid=Long.valueOf(idarr[i]);
+                    PropertyItemVO piv=map.get(pid);
+                    if(piv==null){//说明不在props总串里,略去
+                        log.error("cid="+psv.getCid()+"  pid="+pid+" in input but not in props");
+                        continue;
+                    }
+                    //如果用户把颜色命名为颜色,就是出现"颜色;颜色;颜色"的情况,需要把后面一个颜色转义
+                    String hereStr=strarr[i];
+                    if(hereStr.contains(piv.getName()+";"+piv.getName()+";"+piv.getName())){
+                        hereStr=hereStr.replace(piv.getName()+";"+piv.getName()+";"+piv.getName(),
+                                piv.getName()+";"+piv.getName()+"####;"+piv.getName());
+                    }
+                    //如果条数大于自定义vid数,那么要替换一下
+                    if(nonpv.get(pid)!=null&&hereStr.split(";").length>nonpv.get(pid).size()){
+                        hereStr=hereStr.replace(piv.getName()+";","");//把颜色;这种去掉
+                    }
+                    String[] strs=hereStr.split(";");
+                    for(String s:strs){
+                        Long vid=nonpv.popOneVid(pid);
+                        if(vid==null){
+                            continue;
+                        }
+                        PropertyValueVO ipvv=new PropertyValueVO();
+                        ipvv.setVid(vid);
+                        ipvv.setSelected(true);
+                        ipvv.setName(s.endsWith("####")?s.substring(0,s.length()-4):s);
+                        //设置给属性
+                        Long upvid=piv.addPropValueReturnId(ipvv);
+                        if(!upvid.equals(ipvv.getVid())){
+                            nonpv.pushVidFirst(pid,vid);//没成功添加进去
+                            nonpv.popOneVid(upvid);
+                        }
+                    }
+                }
+            }else{
+                log.error("input length error!!! cid="+psv.getCid()+"  inputIds="+inputIds+" inputStrs="+inputStr);
+            }
+        }
+        //制作SKU显示列表,按页面上每一个tr为一个对象,如果有一项销售属性没有选择,sku没有
+        //先拿出已选的颜色,再选出已选的销售属性
+        //把销售属性搞出来
+        psv.setSkus(calculateSku(psv.getColor(),psv.getSaleProps(),itemId));
+        //如果通过上面,最终都没有name的,写入日志
+        //处理材质成份
+        if(psv.getProperties()==null){
+            psv.setProperties(new ArrayList<PropertyItemVO>());
+        }
+        for(PropertyItemVO piv:psv.getProperties()){
+            //如果是材质,转一下pv
+            if(piv.getType().equals(PropType.MATERIAL)){
+                for(PropertyValueVO pvv:piv.getValues()){
+                    pvv.calMaterialPecent();//计算出材质成份map
+                }
+//                piv.addEmpty();
+                break;
+            }
+        }
+        if(nonpv.size()>0){
+            log.error("cid="+psv.getCid()+"  nonpv="+nonpv+" not have name");
+        }
+        return psv;
+    }
+
+    /**
      * 计算sku
      * @param color 颜色
      * @param sales 其它销售属性
@@ -76,25 +294,25 @@ public class PropsService {
         List<PropertyItemVO> salesselected=new ArrayList<>();
         //有可能没有销售属性
         if(sales!=null)
-        for(PropertyItemVO s:sales){
-            PropertyItemVO st= (PropertyItemVO) s.clone();
-            List<PropertyValueVO> stpvv=new ArrayList<>();
-            if (st.getValues() == null) {
-                st.setValues(new ArrayList<PropertyValueVO>());
-            }
-            for(PropertyValueVO pvv:st.getValues()){
-                if(pvv.isSelected()){
-                    stpvv.add(pvv);
+            for(PropertyItemVO s:sales){
+                PropertyItemVO st= (PropertyItemVO) s.clone();
+                List<PropertyValueVO> stpvv=new ArrayList<>();
+                if (st.getValues() == null) {
+                    st.setValues(new ArrayList<PropertyValueVO>());
+                }
+                for(PropertyValueVO pvv:st.getValues()){
+                    if(pvv.isSelected()){
+                        stpvv.add(pvv);
+                    }
+                }
+                //如果这个销售属性没选,无法生成SKU
+                if(stpvv.size()==0){
+                    return null;
+                }else{
+                    st.setValues(stpvv);
+                    salesselected.add(st);
                 }
             }
-            //如果这个销售属性没选,无法生成SKU
-            if(stpvv.size()==0){
-                return null;
-            }else{
-                st.setValues(stpvv);
-                salesselected.add(st);
-            }
-        }
         //如果color没有,sales也没有,那么直接不用算
         if(colorSelected==null&&salesselected.size()==0){
             return null;
